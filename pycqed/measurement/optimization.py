a36f7af39333922afcac45093ab37b2635f4b72d
Stefania Lazar <stefania.lazar@phys.ethz.ch>
2021-07-06 18:42:59 +0200

    optimization.py: replaces bfgs optimizer with minimize in neural_network_opt

--- pycqed/measurement/optimization.py
+++ pycqed/measurement/optimization.py
@@ -459,22 +459,19 @@ def neural_network_opt(fun, training_grid, target_values = None,
         x_init = np.zeros(n_features)
         #The data is centered. No values above -1,1 should be encountered
         bounds=[(-1.,1.) for i in range(n_features)]
-        res = fmin_l_bfgs_b(estimator_wrapper, x_init, bounds=bounds,
-                            approx_grad=True)
+        res = minimize(estimator_wrapper, x_init, method='Nelder-Mead')
     else:
         print('x_init minimizer:', x_init)
         for it in range(n_features):
             x_init[it] = (x_init[it]-input_feature_means[it])/input_feature_ext[it] # scale initial value
         bounds=[(-1.,0.5) for i in range(n_features)]
-        res = fmin_l_bfgs_b(estimator_wrapper, x_init, approx_grad=True, bounds=bounds)
-        # res = minimize(estimator_wrapper, x_init, method='Nelder-Mead')
+        res = minimize(estimator_wrapper, x_init, method='Nelder-Mead')
         # res = [res.x]
         print('result:', res)
         # print(res)
 
 
-    # result = res.x
-    result = res[0]
+    result = res.x
     opti_flag = True
 
     for it in range(n_features):
