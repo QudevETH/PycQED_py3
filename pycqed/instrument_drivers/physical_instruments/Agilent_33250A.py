2ff50ab9814511a7d6e49ae5ba64f73ce960a406
Christoph Hellings <chellings@phys.ethz.ch>
2021-10-10 13:51:13 +0200

    Agilent_33250A: changes from pycqedscripts repository This is the version of the driver that we have been using on the setups.

--- pycqed/instrument_drivers/physical_instruments/Agilent_33250A.py
+++ pycqed/instrument_drivers/physical_instruments/Agilent_33250A.py
@@ -1,5 +1,5 @@
 from qcodes import VisaInstrument
-from qcodes.utils.validators import Numbers, Enum
+from qcodes.utils.validators import Numbers, Enum, MultiType
 
 
 class Agilent_33250A(VisaInstrument):
@@ -10,11 +10,14 @@ class Agilent_33250A(VisaInstrument):
 
     Only most commonly used commands of the device integrated at this stage.
     """
-    def __init__(self, name ,adress,**kwargs):
+    def __init__(self, name ,address,**kwargs):
 
-        super().__init__(name,adress,**kwargs)
+        super().__init__(name,address,**kwargs)
 
-        self.add_parameter('frequency',
+        # set output units to voltage peak-to-peak
+        self.visa_handle.write('VOLT:UNIT VPP')
+
+        self.add_parameter(name='frequency',
                            label='Frequency',
                            unit='Hz',
                            get_cmd='FREQ?',
@@ -23,49 +26,91 @@ class Agilent_33250A(VisaInstrument):
                            set_parser=float,
                            vals=Numbers(min_value=1e-6,
                                         max_value=80e6),
-                           docstring=('Command for setting the pulse frequency. Min value: 1e-6Hz'
-                                      'Max value: 80e6Hz'))
-
-        self.add_parameter('pulse_shape',
-                           label='Pulse_shape',
+                           docstring=('Command for setting the pulse frequency. Min value: 1 uHz, Max value: 80 MHz'))
+        self.add_parameter(name='pulse_shape',
+                           label='Pulse shape',
                            get_cmd='FUNC?',
                            set_cmd='FUNC {}',
                            vals=Enum('SIN','SQU'),
-                           docstring=('Command for setting the desired pulse shape. '
-                                      'Currently supportet: sinus, square.'))
-
-        self.add_parameter('pulse_width',
-                           label='Pulse_width',
+                           docstring=('Command for setting the desired pulse shape. Currently supported: SIN, SQU.'))
+        self.add_parameter(name='pulse_width',
+                           label='Pulse width',
                            get_cmd='PULS:WIDT?',
                            set_cmd='PULS:WIDT {}',
                            get_parser=float,
                            set_parser=float,
                            unit='s',
-                           vals=Numbers(min_value=8e-9,max_value=2e3),
-                           docstring=('Command for setting the desired pulse width. Min value: 8e-9s'
-                                      'Max value: 2000s.'))
-
-        self.add_parameter('amplitude',
-                           label='Amplitude',
-                           unit='V',
-                           get_cmd='VOLT?',
-                           set_cmd='VOLT',
+                           vals=Numbers(min_value=8e-9, max_value=2e3),
+                           docstring=('Command for setting the desired pulse width. Min value: 8 ns, Max value: 2000 s.'))
+        self.add_parameter('pulse_period',
+                           label='Pulse period',
+                           get_cmd='PULS:PER?',
+                           set_cmd='PULS:PER {}',
                            get_parser=float,
                            set_parser=float,
-                           vals=Numbers(min_value=1e-3,max_value=10),
-                           docstring=('Command for setting the desired pulse amplitude. Min value: 1e-3V'
-                                      'Max value: 10V.'))
+                           unit='s',
+                           vals=Numbers(min_value=20e-9, max_value=2e3),
+                           docstring=('Command for setting the desired pulse period. Min value: 20 ns, Max value: 2000 s.'))
+        self.add_parameter(name='amplitude',
+                           label='Amplitude',
+                           unit='Vpp',
+                           get_cmd='VOLT?',
+                           set_cmd='VOLT {}',
+                           get_parser=float,
+                           set_parser=float,
+                           vals=Numbers(min_value=1e-3, max_value=20),
+                           docstring=('Command for setting the desired pulse amplitude. Min value for 50 Ohm load: 1 mVpp, Max value for 50 Ohm load: 10 Vpp. Min value for high impedance load: 2 mVpp, Max value for high impedance load: 20 Vpp.'))
+        self.add_parameter('offset',
+                           label='Offset',
+                           unit='V',
+                           get_cmd='VOLT:OFFS?',
+                           set_cmd='VOLT:OFFS {}',
+                           get_parser=float,
+                           set_parser=float,
+                           vals=Numbers(min_value=-10, max_value=10),
+                           docstring=('Command for setting the desired dc offset. Min value for 50 Ohm load: -10 V, Max value for 50 Ohm load: 10 V. Min value for high impedance load: -10 V, Max value for high impedance load: 10 Vpp.'))
         self.add_parameter(name='output',
                            get_cmd='OUTP?',
                            set_cmd='OUTP {}',
                            val_mapping={'OFF': 0,
                                         'ON': 1},
                            docstring=('Command for switching on/off the device output.'))
-
         self.add_parameter(name='output_sync',
                            get_cmd='OUTP:SYNC?',
                            set_cmd='OUTP:SYNC {}',
                            val_mapping={'OFF': 0,
                                         'ON': 1},
                            docstring='Command for switching on/off the device output synchronization.')
+        self.add_parameter(name='load_impedance',
+						   label='Load impedance',
+						   unit='Ohm',
+						   get_cmd='OUTP:LOAD?',
+						   set_cmd='OUTP:LOAD {}',	
+                           get_parser=float,
+                           set_parser=float,
+                           vals=MultiType(Numbers(min_value=1, max_value=10e3), Enum('INF')),
+                           docstring=("Command for setting the load impedance in Ohms. Min value: 1 Ohm, Max value: 10 kOhm or 'INF'"))						   
+        
         self.connect_message()
+    
+    def reset(self):
+        self.write('*RST')
+
+    def start(self, **kw):
+        """
+        :param kw: currently ignored, added for compatibilty with other
+            instruments that accept kwargs in start().
+        """
+        self.write('OUTP OFF')
+        self.write('BURS:STAT OFF')
+        self.write('BURS:MODE TRIG')
+        self.write('BURS:NCYC INF')
+        self.write('TRIG:SOUR BUS')
+
+        self.write('BURS:STAT ON')
+        self.write('OUTP ON')
+        self.write('TRIG')
+    
+    def stop(self):
+        self.write('OUTP OFF')
+    
