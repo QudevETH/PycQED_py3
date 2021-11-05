860d4c4ea35c863e72aff62642164a3fff031c5d
Christoph Hellings <chellings@phys.ethz.ch>
2021-11-05 20:50:58 +0100

    Sequence.__getitem__: allow indexing by a list and added error handling

--- pycqed/measurement/waveform_control/sequence.py
+++ pycqed/measurement/waveform_control/sequence.py
@@ -484,14 +484,24 @@ class Sequence:
         """
         Return a segment by its name or index, or a list of segments
         corresponding to a slice.
-        :param i: (str, int, slice) Name, index, or slice identifying the
-            segment(s).
+        :param i: (str, int, slice, list) Name, index, slice,
+            or list of names/indices identifying the segment(s).
         :return: segment or list of segments
         """
-        if isinstance(i, str):
+        if isinstance(i, list):
+            return [self[i] for i in i]
+        elif isinstance(i, str):
+            if i not in self.segments:
+                raise KeyError(f'No segment with name "{i}" in the sequence '
+                               f'{self.name}.')
             return self.segments[i]
         else:
-            return list(self.segments.values())[i]
+            try:
+                return list(self.segments.values())[i]
+            except IndexError:
+                raise IndexError(
+                    f'Segment index {i} out of range. The sequence '
+                    f'{self.name} has {len(self.segments)} segments.')
 
     def plot(self, segments=None, show_and_close=True, **segment_plot_kwargs):
         """
