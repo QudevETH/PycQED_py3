0d07810c5ccf9ab6181637f642c481df760e4cfd
Christoph Hellings <chellings@phys.ethz.ch>
2021-12-11 15:33:19 +0100

    utils ro_flux_tmp_vals: cleanup - renamed qb to qubit (convention as in other new functions) - cleaned up code structure and formatting - improved comments and docstring - removed unneeded import

--- pycqed/utilities/flux_assisted_readout.py
+++ pycqed/utilities/flux_assisted_readout.py
@@ -1,66 +1,89 @@
-from pycqed.utilities.general import temporary_value
+def ro_flux_tmp_vals(qb, v_park=None, use_ro_flux=None, **kw):
+    """Create temporary values for flux-pulse-assisted read-out
 
-def ro_flux_tmp_vals(qubit, v_park=None, use_ro_flux=None, **kw):
-    """
-    Creates temporary values for flux assisted read-out
+    This function can calculate a temporary ro_flux_amplitude for a
+    temporary parking position v_park, based on the information provided in
+    the following qcodes parameters of the qubit object, which need to be
+    set properly prior to calling the function:
+    - flux_parking
+    - flux_amplitude_bias_ratio
+    - fit_ge_freq_from_dc_offset
+    - ro_flux_amplitude if the qubit was already configured for fp-assisted RO
+
+    If the qubit was not yet configured for fp-assisted RO, it can be
+    temporarily configured for it (if use_ro_flux is True) based on default
+    parameter settings or on settings provided as kw. Note that the parameter
+    flux_pulse_channel needs to configured before calling the function in this
+    case.
+
+    The function can also be used to temporarily deactivate fp-assisted RO.
 
     Args:
-        qubit: qubit to be read-out
-        v_park: voltage where the read-out happens (usually the sweet spot is a good place to do this)
-        use_ro_flux: ...
-        kw:
-            ro_flux_extend_start
-            ro_buffer_length_start
-            ro_flux_extend_end
-            ro_buffer_length_end
-            ro_flux_gaussian_filter_sigma
+        qb (QuDev_transmon): qubit for which the fp-assisted RO should be
+            configured temporarily
+        v_park (float, None): DC bias voltage applied to the qubit at the
+            moment the read-out happens. The default value None means that
+            the the usual DC bias corresponding to qb.flux_parking() is
+            applied.
+        use_ro_flux (bool, None): whether fp-assisted RO should be used. True
+            (False) means that the fp-assisted RO will be temporarily activated
+            (deactivated) if it is usually inactive (active). The default
+            value None means that the temporary values will not change
+            whether or not fp-assisted RO is active, but only adjust the
+            amplitude if it is active.
+        kw: If use_ro_flux is True for a qubit for which fp-assisted RO is
+            usually not used, the following keyword arguments can be used
+            to overwrite the default temporary settings:
+            - ro_flux_extend_start
+            - ro_buffer_length_start
+            - ro_flux_extend_end
+            - ro_buffer_length_end
+            - ro_flux_gaussian_filter_sigma
 
-            Note that the above key word arguments will be used as temporary value
-
-    Example:
+    Example::
         with temporary_value(*ro_flux_tmp_vals(qubit,
                                                v_park=v_park,
                                                use_ro_flux=True)):
-
-            [some code that reads out qubit state]
+            # code that runs experiments using the temporary RO settings
 
     """
-
-    if use_ro_flux is None:
-        use_ro_flux = qubit.ro_pulse_type() == 'GaussFilteredCosIQPulseWithFlux'
-
     tmp_vals = []
+    if use_ro_flux is None:
+        use_ro_flux = qb.ro_pulse_type() == 'GaussFilteredCosIQPulseWithFlux'
 
     if use_ro_flux:
-        if qubit.ro_pulse_type() == 'GaussFilteredCosIQPulse':
+        if qb.ro_pulse_type() == 'GaussFilteredCosIQPulse':
+            # temporarily activate and configure fp-assisted RO
             tmp_vals += [
-                (qubit.ro_pulse_type, kw.pop('ro_pulse_type', 'GaussFilteredCosIQPulseWithFlux')),
-                (qubit.ro_flux_extend_start, kw.pop('ro_flux_extend_start', 30e-9)),
-                (qubit.ro_buffer_length_start, kw.pop('ro_buffer_length_start', 40e-9)),
-                (qubit.ro_flux_extend_end, kw.pop('ro_flux_extend_end', 120e-9)),
-                (qubit.ro_buffer_length_end, kw.pop('ro_buffer_length_end', 130e-9)),
-                (qubit.ro_flux_gaussian_filter_sigma, kw.pop('ro_flux_gaussian_filter_sigma', .5e-9)),
-                (qubit.ro_flux_channel, qubit.flux_pulse_channel()),
+                (qb.ro_pulse_type, 'GaussFilteredCosIQPulseWithFlux'),
+                (qb.ro_flux_extend_start, kw.pop(
+                    'ro_flux_extend_start', 30e-9)),
+                (qb.ro_buffer_length_start, kw.pop(
+                    'ro_buffer_length_start', 40e-9)),
+                (qb.ro_flux_extend_end, kw.pop(
+                    'ro_flux_extend_end', 120e-9)),
+                (qb.ro_buffer_length_end, kw.pop(
+                    'ro_buffer_length_end', 130e-9)),
+                (qb.ro_flux_gaussian_filter_sigma, kw.pop(
+                    'ro_flux_gaussian_filter_sigma', .5e-9)),
+                (qb.ro_flux_channel, qb.flux_pulse_channel()),
             ]
-            if v_park is not None:
-                ro_flux_amplitude = 0
-
-            else:
-                tmp_vals += [
-                    (qubit.ro_flux_amplitude, 0),
-                ]
-
-        elif v_park is not None:
-            ro_flux_amplitude = qubit.ro_flux_amplitude()
+            ro_flux_amp = 0
+            if v_park is None:
+                # no new DC bias: configure a zero-amplitude flux pulse
+                tmp_vals += [(qb.ro_flux_amplitude, ro_flux_amp)]
+        else:  # fp-assisted RO already used
+            ro_flux_amp = qb.ro_flux_amplitude()
 
         if v_park is not None:
-            delta_v = v_park - qubit.calculate_voltage_from_flux(qubit.flux_parking())
-            ro_flux_amp = ro_flux_amplitude - delta_v * qubit.flux_amplitude_bias_ratio()
-            tmp_vals += [(qubit.ro_flux_amplitude, ro_flux_amp)]
+            # calculate and configure amplitude for new DC bias
+            delta_v = v_park - qb.calculate_voltage_from_flux(
+                qb.flux_parking())
+            ro_flux_amp -= delta_v * qb.flux_amplitude_bias_ratio()
+            tmp_vals += [(qb.ro_flux_amplitude, ro_flux_amp)]
 
-    elif (not use_ro_flux) and qubit.ro_pulse_type() == 'GaussFilteredCosIQPulseWithFlux':
-        tmp_vals += [
-            (qubit.ro_pulse_type, 'GaussFilteredCosIQPulse'),
-        ]
+    elif qb.ro_pulse_type() == 'GaussFilteredCosIQPulseWithFlux':
+        # temporarily deactivate fp-assisted RO
+        tmp_vals += [(qb.ro_pulse_type, 'GaussFilteredCosIQPulse')]
 
     return tmp_vals
