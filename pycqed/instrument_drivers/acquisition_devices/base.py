2a30b6ce3c0ab9fd24bdc4606a0005d93312c207
Christoph Hellings <chellings@phys.ethz.ch>
2022-01-17 16:48:24 +0100

    AcqDev: added/improved docstrings

--- pycqed/instrument_drivers/acquisition_devices/base.py
+++ pycqed/instrument_drivers/acquisition_devices/base.py
@@ -7,10 +7,38 @@ log = logging.getLogger(__name__)
 
 
 class AcquisitionDevice():
-    """
-    TODO
+    """Base class for a standardized acquisition device driver interface.
 
-    note: the acq dev should have a qcode param timeout
+    This class is not meant to be instantiated, but is only meant to be used
+    as a parent class as described in the following:
+    Child classes should inherit via multi-inheritance from the underlying
+    qcodes driver as first parent and from this class as subsequent parent.
+    The init of the child class has to explicitly call the init of this base
+    class after calling the super init since the qcodes intrument (first
+    parent) will not forward the super call. In the list of attributes,
+    (*) indicates constants that are meant to be overwritten by child
+    classes if needed.
+
+    Attributes:
+        n_acq_units (int): number of physical acquisition units (*)
+        n_acq_channels (int): number of integration channels per acquisition
+            unit (*)
+        acq_length_granularity (int): indicates that the number of samples
+            in an acquired signal must be a multiple of this number (*)
+        acq_sampling_rate (float): sampling rate of the acquisition units in
+            seconds (*)
+        acq_weights_n_samples (int): number of samples of auto-generated
+            integration weights (*)
+        acq_Q_sign (1 or -1): sign of auto-generated integration weights for
+            the second weighted-integration channel in a pair (*)
+        allowed_weights_types (list of str): allowed types of auto-generated
+            integration weights (*)
+        allowed_modes (dict): each key is a str supported as mode argument
+            of acquisition_initialize, and the corresponding value is a list
+            of str, which are supported as data_type argument of
+            acquisition_initialize if that mode is used (*)
+        lo_freqs (list of float/None): LO frequencies of the internal or
+            external LOs of all acquisition units, see set_lo_freq
     """
 
     n_acq_units = 1
@@ -24,6 +52,11 @@ class AcquisitionDevice():
                              'DSB', 'DSB2', 'square_rot']
 
     def __init__(self, *args, **kwargs):
+        """Init of the common part of all acquisition devices.
+
+        Accepts *args and **kwargs for compatibility in super calls,
+        but currently ignores them.
+        """
         if not hasattr(self, 'IDN'):
             raise AttributeError(
                 f'{repr(self)} was not properly initialized as a qcodes '
@@ -46,11 +79,50 @@ class AcquisitionDevice():
                 vals=validators.Ints())
 
     def set_lo_freq(self, acq_unit, lo_freq):
+        """Set the local oscillator frequency used for an acquisition unit.
+
+        The base AcquisitionDevice only stores the information in the property
+        lo_freqs. Child classes, in particular for instruments with an
+        internal LO, can implement further functionality by accessing this
+        property or by overriding set_lo_freq.
+
+        Remark: This method is called from QuDev_transmon.prepare.
+
+        Args:
+             acq_unit (int): the acquisition unit for which the LO frequency
+                should be set.
+            lo_freq (float): the LO frequency
+        """
         self.lo_freqs[acq_unit] = lo_freq
 
     def acquisition_initialize(self, channels, n_results, averages, loop_cnt,
                                mode, acquisition_length, data_type=None,
                                **kwargs):
+        """Initialize the acquisition device.
+
+        Configures the acquisition device for an experiment and performs
+        tasks that need to be done once per experiment (i.e., not repeatedly
+        in sweeps). The base method stores and validates acquisition
+        settings. Further functionality can be implemented in child classes.
+
+        Args:
+            channels (list of tuple of int): Channels on which the
+                acquisition should be performed. A channel is identified by
+                two ints in a tuple. The first int is the index of the
+                acquisition unit, the second int is a logical index within
+                the physical unit (e.g., the index of a weighted-integration
+                channel).
+            n_results (int): number of acquisition elements
+            averages (int): number of repetitions for averaging
+            loop_cnt (int): total number of repetitions (averaging & shots)
+            mode (str): acquisition modes. One of the keys of
+                self.allowed_modes
+            acquisition_length (float): length of the acquired signals in
+                seconds
+            data_type (str): data type. One of the str listed in
+                self.allowed_modes for the chosen mode.
+            **kwargs: currently ignored
+        """
 
         self._acquisition_nodes = deepcopy(channels)
         self._acq_length = acquisition_length
@@ -73,17 +145,22 @@ class AcquisitionDevice():
         self._check_hardware_limitations()
 
     def acquisition_finalize(self):
+        """Finalize the acquisition device.
+
+        Performs cleanup at the end of an experiment (i.e., not repeatedly in
+        sweeps). No actions by default, can be overridden in child classes.
+        """
         pass
 
     def _reset_n_acquired(self):
         """Reset quantities that are needed by acquisition_progress.
+
+        No actions by default, can be overridden in child classes.
         """
         pass
 
     def _check_allowed_acquisition(self):
-        """
-        Check whether the specified mode and data_type is supported by the
-        acquisition device.
+        """Check whether the specified mode and data_type is supported.
         """
 
         if self._acq_mode not in self.allowed_modes:
@@ -96,61 +173,158 @@ class AcquisitionDevice():
                              f'acquisition.')
 
     def acquisition_nodes(self):
+        """Get a list of the currently used acquisition nodes.
+
+        Acquisition nodes are unique identifiers of the acquisition
+        channels. By default, they are in the same format as used in the
+        channels argument of acquisition_initialize, but a child class could
+        replace them by an actual hardware address (such as node names in
+        ZI devices).
+        """
         return deepcopy(self._acquisition_nodes)
 
     def acquisition_progress(self):
-        return 0  # no intermediate progress information available
+        """Gets the number of totally acquired data points.
+
+        Here, total refers to counting each acquisition element as many
+        times as it has been acquired already (e.g., in a loop of
+        repetitions for averaging), i.e., 100% progress corresponds to the
+        this method reporting self._acq_n_results * self._acq_loop_cnt.
+
+        The method always returns 0 indicating that no intermediate progress
+        information is available. If the child class does not overwrite the
+        method with a concrete implementation, progress will be stuck during
+        hard sweeps and will only be updated by MC after the hard sweep.
+        """
+        return 0
 
     def prepare_poll(self):
-        """
+        """Final preparations for an acquisition.
+
         This function is called by PollDetector.poll_data right before
         starting the AWGs (i.e., we can rely on that the AWGs have already
-        been stopped) for final preparations. This is the place to arm the
-        acquisition device (i.e., let it wait for triggers).
+        been stopped in case this is needed) for final preparations. This is
+        the place to arm the acquisition device (i.e., let it wait for
+        triggers). Unlike acquisition_initialize, this function is called
+        multiple times in a soft sweep.
         """
         pass
 
     def poll(self, poll_time=0.1):
+        """Poll data from the acquisition device.
+
+        The method needs to be implemented in a child class.
+
+        Args:
+            poll_time (float): time in seconds that the device should wait
+            for new data. If no new data is available by the end of this
+            time span, an empty data set is returned.
+
+        Returns:
+             A dict, where each key is an acquisition node (see
+             self.acquisition_nodes), and the corresponding value is a list
+             of numpy arrays containing new data.
+        """
         raise NotImplementedError(f'poll not implemented '
                                   f'for {self.__class__.__name__}')
 
     def start(self, **kw):
-        """
-        Start the built-in AWG (if present).
-        :param kw: currently ignored, added for compatibilty with other
-            instruments that accept kwargs in start().
+        """ Start the built-in AWG (if present).
+
+        Args:
+            **kw: currently ignored, added for compatibilty with other
+                instruments that accept kwargs in start().
         """
         pass  # default acquisition device does not have an AWG
 
     def stop(self):
-        """
-        Stop the built-in AWG (if present).
+        """Stop the built-in AWG (if present).
         """
         pass  # default acquisition device does not have an AWG
 
     def _check_hardware_limitations(self):
-        """
-        This method should be implemented in child classes to check whether
-        the measurement settings are supported by the hardware.
+        """Check whether acquisition settings are supported by the hardware
+
+        No checks by default. Should be implemented in child classes to check
+        whether the measurement settings are supported by the hardware.
         """
         pass
 
     def get_value_properties(self, data_type='raw', acquisition_length=None):
+        """Returns properties of the returned values of a given data type.
+
+        Args:
+            data_type (str): data type. One of the str listed in
+                self.allowed_modes for the chosen mode.
+            acquisition_length (float): length of the acquired signals in
+                seconds
+
+        Returns:
+            A dict with the following entries:
+            value_unit: the base unit of the returned values (by default a.u.
+                unless overridden by the base classes).
+            scaling_factor: a scaling factor that needs to be applied to the
+                returned values in order to bring them into their base unit
+                (by default 1, unless overridden by the base classes).
+        """
         return {'value_unit': 'a.u.', 'scaling_factor': 1}
 
     def correct_offset(self, channels, data):
+        """Correct potential offsets in the data.
+
+        By default, an offset of 0 is assumed. Can be overridden by a child
+        class.
+
+        Args:
+            channels (list of tuple of int): channels for which corrections
+                need to be applied (channel format as in the docstring of
+                acquisition_initialize)
+            data (dict): each key is a channel from the list channels,
+                and the corresponding value is a numpy array of data.
+
+        Returns:
+            A dict in the same format of data, after correcting the offsets.
+        """
         return data
 
     def get_lo_sweep_function(self, acq_unit, ro_mod_freq):
+        """Return a sweep function for sweeping the internal LO
+
+        Needs to be implemented in the child class for acquisition devices
+        with an internal LO. No implementation needed if an external LO is
+        used.
+
+        Args:
+             acq_unit (int): the acquisition unit for which the LO frequency
+                should be swept.
+            ro_mod_freq (float): the desired intermediate frequency (IF) in
+                Hertz. Depending on the implementation in the child class,
+                the frequency sweep will either be done by a pure LO sweep
+                for this fixed IF, or it will be implemented as a combined
+                sweep of LO and IF in a way that the IF is as close as
+                possible to the provided value, while accounting for
+                hardware limitations of the internal LO.
+
+        Returns:
+            A sweep function object for the frequency sweep.
+        """
         raise NotImplementedError(f'{self.__class__.__name__} does not have '
                                   f'an internal LO.')
 
     def convert_time_to_n_samples(self, time, align_acq_granularity=False):
-        """
+        """Convert from time duration to number of samples.
+
         Calculate the number of samples that the specified time corresponds to
         based on the acquisition sampling rate of the device.
-        :param time: time in seconds (float)
-        :return: number of samples (int)
+
+        Args:
+            time (float): time in seconds
+            align_acq_granularity (bool): Whether the calculated number of
+                samples should account for acquisition length granularity by
+                shortening the length if needed (default: False).
+
+        Returns:
+            number of samples (int)
         """
         n_samples = int(round(time * self.acq_sampling_rate))
         if align_acq_granularity:
@@ -162,6 +336,7 @@ class AcquisitionDevice():
         """
         Calculate the duration that the specified number of samples corresponds
         to based on the acquisition sampling rate of the device.
+
         :param n_samples: number of samples (int)
         :return: time in seconds (float)
         """
@@ -288,11 +463,32 @@ class AcquisitionDevice():
             raise KeyError('Invalid weights type: {}'.format(weights_type))
 
     def _acquisition_set_weight(self, channel, weight):
+        """Set a vector of integration weights for a channel.
+
+        This method needs to be implemented in each acquisition device child
+        class that allows setting integration weights.
+
+        Args:
+             channel (tuple of int): Channel for which the weight vector
+             should be set (channel format as in acquisition_set_weights).
+            weight (tuple of numpy array): vector of complex acquisition
+                weights. The first vector in the tuple is the real part, and
+                the second vector in the tuple is the imaginary part.
+        """
         raise NotImplementedError(f'_acquisition_set_weight not implemented '
                                   f'for {self.__class__.__name__}')
 
 
 class ZI_AcquisitionDevice(AcquisitionDevice):
+    """Base class for ZI acquisition devices.
+
+    This class extends AcquisitionDevice with methods that are common to
+    multiple ZI devices.
+
+    FIXME: Check whether the overlap between UHF and SHF is really high
+        enough to justify such a class.
+    """
+
     def get_value_properties(self, data_type='raw', acquisition_length=None):
         properties = super().get_value_properties(
             data_type=data_type, acquisition_length=acquisition_length)
