f14402e1f5d5557e5ae446b71211c86e4880239f
Colin <cscarato@phys.ethz.ch>
2022-01-26 15:04:33 +0100

    QDacSmooth: resolve confusing nomenclature regarding "current"

--- pycqed/instrument_drivers/physical_instruments/QDevil_QDac.py
+++ pycqed/instrument_drivers/physical_instruments/QDevil_QDac.py
@@ -136,23 +136,23 @@ class QDacSmooth(QDac):
 
         return self.channels[chan].v()
 
-    def get_current_fluxline_voltages(self):
+    def get_fluxline_voltages(self):
         """
         Convenience method to retrieve the fluxline voltages.
 
         Returns:
-            dict: The current fluxline voltages.
+            dict: The current/actual fluxline voltages.
         """
         return {ch_name: self.channels[chan].v()
                 for ch_name, chan in zip(self.channel_map.values(),
                                          range(self.num_chans))}
 
-    def get_current_channel_voltages(self):
+    def get_channel_voltages(self):
         """
         Convenience method to retrieve the channel voltages.
 
         Returns:
-            dict: The current channel voltages.
+            dict: The current/actual channel voltages.
         """
         return {chan: self.channels[chan].v()
                 for chan in range(self.num_chans)}
@@ -194,16 +194,16 @@ class QDacSmooth(QDac):
 
         # First set the voltages to zero and then ramp up again when
         # changing modes.
-        current_volt_dict = self.get_current_channel_voltages()
+        original_volt_dict = self.get_channel_voltages()
         zero_volt_dict = {ch: 0.0 for ch in range(self.num_chans)}
         self.set_smooth(zero_volt_dict)
-        for ch_number, ch_volt in current_volt_dict.items():
+        for ch_number, ch_volt in original_volt_dict.items():
             self.channels[ch_number].mode(set_mode)
-            current_volt_dict[ch_number] = _clipto(
+            original_volt_dict[ch_number] = _clipto(
                 ch_number, ch_volt,
                 self.vranges[ch_number+1][set_mode.value.v]['Min'],
                 self.vranges[ch_number+1][set_mode.value.v]['Max'])
-        self.set_smooth(current_volt_dict)
+        self.set_smooth(original_volt_dict)
 
     def _update_cache(self, *args, **kwargs):
         super()._update_cache(*args, **kwargs)
