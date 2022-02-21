76eea6e093d694f99028835f87a6326fcfa99ca5
MichKe95 <Kerschbaum.1995@gmail.com>
2022-02-21 09:07:47 +0100

    Changes in MicrowaveSwitch_ArduinoMega2560.py according to MR !96.

--- pycqed/instrument_drivers/physical_instruments/MicrowaveSwitch_ArduinoMega2560.py
+++ pycqed/instrument_drivers/physical_instruments/MicrowaveSwitch_ArduinoMega2560.py
@@ -6,56 +6,58 @@ import telnetlib
 import serial
 import time
 
-
-class MicrowaveSwitch_SP6T_ArduinoMega2560(Instrument):
-    '''
+class MicrowaveSwitchSP6TArduinoMega2560(Instrument):
+    """
     QCodes driver for a SP6T switch controlled via an Arduino Mega 2560
-    '''
+    """
 
-    def __init__(self, name, address):
-        Instrument.__init__(self, name)
+    READY_MESSAGE = "READY"
+    _SET_WAIT_TIME = 0.2
+    NB_SWITCH_POS = 6
+
+    def __init__(self, name, address, port=57600):
+        super().__init__(name)
         self.address = address
 
-        self.port = serial.Serial(address, 57600)
+        self.port = serial.Serial(address, port)
 
-        if self.port.read(5) != 'READY'.encode():
-            raise ConnectionError('failed to connect to Arduino')
+        if self.port.read(len(self.READY_MESSAGE)) != self.READY_MESSAGE.encode():
+            raise ConnectionError("failed to connect to Arduino")
 
         self.add_parameter(
-            'switch_mode',
-            label = f'switch_mode of {self.name}',
-            vals = vals.Enum(0, 1, 2, 3, 4, 5, 6),
-            set_cmd = self._set_switch,
-            get_cmd = self._get_switch,
-            docstring = 'possible values: 0 (no channel connected), 1-6'
+            "switch_mode",
+            vals=vals.Enum(*range(self.NB_SWITCH_POS + 1)),
+            set_cmd=self._set_switch,
+            get_cmd=self._get_switch,
+            docstring="possible values: 0 (no channel connected), 1-6"
             )
 
 
     def _set_switch(self, val):
-        for i in range(1, 7):
+        for i in range(1, self.NB_SWITCH_POS+1):
             if i == val:
-                self.port.write('H'.encode() + bytes([i]))
+                self.port.write("H".encode() + bytes([i]))
             else:
-                self.port.write('L'.encode() + bytes([i]))
+                self.port.write("L".encode() + bytes([i]))
 
-        time.sleep(0.2)
+        time.sleep(self._SET_WAIT_TIME)
 
         if val != self._get_switch():
-            raise ValueError('Switch did not switch into the specified state.')
+            raise ValueError("Switch did not switch into the specified state.")
 
 
     def _get_switch(self):
         lst = []
-        for i in range(1, 7):
-            self.port.write('R'.encode() + bytes([i]))
+        for i in range(1, self.NB_SWITCH_POS+1):
+            self.port.write("R".encode() + bytes([i]))
             lst.append(int(self.port.read()[0]))
 
-        if sum(lst) == 5:
+        if sum(lst) == self.NB_SWITCH_POS-1:
             return lst.index(0) + 1
-        elif sum(lst) == 6:
+        elif sum(lst) == self.NB_SWITCH_POS:
             return 0
         else:
-            raise ValueError('Unexpected reading from indicator output!')
+            raise ValueError("Unexpected reading from indicator output!")
 
     def __enter__(self):
         return self
@@ -63,3 +65,4 @@ class MicrowaveSwitch_SP6T_ArduinoMega2560(Instrument):
     def __exit__(self, *args):
         self._set_switch(0)
         self.port.close()
+
