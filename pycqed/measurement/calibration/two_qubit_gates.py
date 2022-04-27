QudevETH/PycQED_py3

commit 3239e3f3df8adcda6f41b40f7cc01e56d8b238c2

MultiTaskingExperiment: add data_to_fit to the metadata only if it is not an empty dict

pycqed/measurement/calibration/two_qubit_gates.py
@@ -162,8 +162,9 @@ class MultiTaskingExperiment(QuantumExperiment):
             'rotate': len(self.cal_states) != 0 and not self.classified,
             'sweep_points': self.sweep_points,
             'ro_qubits': self.meas_obj_names,
-            'data_to_fit': self.data_to_fit,
         })
+        if len(data_to_fit):
+            self.exp_metadata.update({'data_to_fit': self.data_to_fit})
         if kw.get('store_preprocessed_task_list', False) and hasattr(
                 self, 'preprocessed_task_list'):
             tl = [copy(t) for t in self.preprocessed_task_list]

commit 37b76a8e46401b7f1bd41b53b7546fbb9c84dad4

MultiTaskingExperiment: fixed bug introduced by commit 3239e3f3df8adcda6f41b40f7cc01e56d8b238c2 which checks for len of an undefined variable

pycqed/measurement/calibration/two_qubit_gates.py
@@ -163,7 +163,7 @@ class MultiTaskingExperiment(QuantumExperiment):
             'sweep_points': self.sweep_points,
             'ro_qubits': self.meas_obj_names,
         })
-        if len(data_to_fit):
+        if len(self.data_to_fit):
             self.exp_metadata.update({'data_to_fit': self.data_to_fit})
         if kw.get('store_preprocessed_task_list', False) and hasattr(
                 self, 'preprocessed_task_list'):