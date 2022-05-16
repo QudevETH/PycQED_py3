b05212c5e07d7b42d79db9e982ac040169c261e1
Christoph Hellings <chellings@phys.ethz.ch>
2022-05-16 15:45:20 +0200

    QuDev_transmon: added docstring for ro_disable_repeat_pattern

--- pycqed/instrument_drivers/meta_instrument/qubit_objects/QuDev_transmon.py
+++ pycqed/instrument_drivers/meta_instrument/qubit_objects/QuDev_transmon.py
@@ -176,9 +176,12 @@ class QuDev_transmon(Qubit):
                                            ' this qubit.',
                                  label='RO pulse basis rotation dictionary',
                                  vals=vals.Dict())
-        self.add_pulse_parameter('RO', 'ro_disable_repeat_pattern',
-                                 'disable_repeat_pattern',
-                                 initial_value=False, vals=vals.Bool())
+        self.add_pulse_parameter(
+            'RO', 'ro_disable_repeat_pattern', 'disable_repeat_pattern',
+            initial_value=False, vals=vals.Bool(),
+            docstring='True means that repeat patterns are not used for '
+                      'readout pulses of this qubit even if higher layers '
+                      '(like CircuitBuilder) configure a repeat pattern.')
         self.add_pulse_parameter(
             'RO', 'ro_trigger_channels', 'trigger_channels',
             vals=vals.MultiType(vals.Enum(None), vals.Strings(),
