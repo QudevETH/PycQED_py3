qe:spec: bugfix wrong calculation of lo_freq
(cherry picked from commit e027b52d2ea209067d88bf2b4369a10126abedb7)

pycqed/measurement/spectroscopy/spectroscopy.py
@@ -85,7 +85,7 @@ class MultiTaskingSpectroscopyExperiment(CalibBuilder):
                 if len(freqs_all) >= 2:
                     ro_lo_freq = 0.5 * (np.max(freqs_all) + np.min(freqs_all))
                 else:
-                    ro_lo_freq = freqs_all[0] + qubits[0].ro_mod_freq()
+                    ro_lo_freq = freqs_all[0] - qubits[0].ro_mod_freq()
                 configure_qubit_mux_readout(
                     qubits=qubits,
                     lo_freqs_dict={ro_lo: ro_lo_freq},