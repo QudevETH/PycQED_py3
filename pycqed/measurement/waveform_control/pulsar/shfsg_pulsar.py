ff71bf6f2ad198b2c218909bf5035a1f46b56590
Christoph Hellings <chellings@phys.ethz.ch>
2022-08-03 19:49:02 +0200

    SHFSG/HDAWG Pulsar and Segment: revert code that skipped uploading of empty waveforms This was introduced as a way to trigger the SHFSG for hard sweeps, but it turned out that it causes problems because empty waveforms might become non-empty during granularity checks.

--- pycqed/measurement/waveform_control/pulsar/shfsg_pulsar.py
+++ pycqed/measurement/waveform_control/pulsar/shfsg_pulsar.py
@@ -337,11 +337,6 @@ class SHFGeneratorModulePulsar(PulsarAWGInterface, ZIPulsarMixin):
                         wave = (wave[0], None, wave[1], None)
                         ch_has_waveforms[ch1id] |= wave[0] is not None
                         ch_has_waveforms[ch2id] |= wave[2] is not None
-                        # wave contains hashlists and the first entry of
-                        # each hashlist corresponds to the waveform length.
-                        # If this is 0, we do not program the waveform
-                        wave = tuple(None if w is None or not w[0]
-                                     else w for w in wave)
                         if wave == (None, None, None, None):
                             continue
                         if nr_cw != 0:
