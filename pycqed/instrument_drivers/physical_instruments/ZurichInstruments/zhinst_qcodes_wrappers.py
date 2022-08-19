90f47c57cfed870f9ccb8dfc616f893484e88603
Stefania Lazar <stefania.lazar@phys.ethz.ch>
2022-08-19 11:34:44 +0200

    ZHInstMixin: modifies check_server to allow connection to the same server/session

--- pycqed/instrument_drivers/physical_instruments/ZurichInstruments/zhinst_qcodes_wrappers.py
+++ pycqed/instrument_drivers/physical_instruments/ZurichInstruments/zhinst_qcodes_wrappers.py
@@ -24,16 +24,17 @@ class ZHInstMixin:
         return self.session.daq_server
 
     def check_server(self, kwargs):
-        if kwargs.pop('server', '') == 'emulator':
+        # Note: kwargs are passed without ** in order to allow modifying them.
+        if kwargs.pop('server', None) == 'emulator':
             from pycqed.instrument_drivers.physical_instruments \
                 .ZurichInstruments import ZI_base_qudev as zibase
             from zhinst.qcodes import session as ziqcsess
-            daq = zibase.MockDAQServer(kwargs.get('host', 'localhost'),
-                                       port=kwargs.get('port', 8004),
-                                       apilevel=5)
-            self._session = ziqcsess.Session(
+            daq = zibase.MockDAQServer.get_instance(
+                kwargs.get('host', 'localhost'),
+                port=kwargs.get('port', 8004))
+            self._session = ziqcsess.ZISession(
                 server_host=kwargs.get('host', 'localhost'),
-                connection=daq)
+                connection=daq, new_session=False)
             return daq
 
 
