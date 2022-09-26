1baa543f63a27031c4709f47ebdbb02189214cbd
bguttel <bguttel@phys.ethz.ch>
2022-09-26 11:59:48 +0200

    Extract `InitialQubitParking` into its own module.

--- pycqed/measurement/calibration/automatic_calibration_routines/__init__.py
+++ pycqed/measurement/calibration/automatic_calibration_routines/__init__.py
@@ -2,16 +2,18 @@ from .base.base_automatic_calibration_routine import AutomaticCalibrationRoutine
 from .base.base_settings_dictionary import SettingsDictionary
 
 from .single_qubit_routines import (
-    InitialQubitParking,
     AdaptiveQubitSpectroscopy,
     PiPulseCalibration,
     FindFrequency,
     SingleQubitCalib
 )
-from .hamiltonian_fitting_routines import (
-    HamiltonianFitting
-)
+
+from .initial_qubit_parking import InitialQubitParking
 
 from .park_and_qubit_spectroscopy import ParkAndQubitSpectroscopy
 
 from .initial_hamiltonian_model import PopulateInitialHamiltonianModel
+
+from .hamiltonian_fitting_routines import (
+    HamiltonianFitting
+)
