9a6d9d51833fee08fdf756fadff436d4316777b8
Nathan Lacroix <lna@student.ethz.ch>
2022-10-03 14:38:53 +0200

    Create pycqed wrapper for QcodesInstrumentModule

--- pycqed/instrument_drivers/instrument.py
+++ pycqed/instrument_drivers/instrument.py
@@ -1,5 +1,5 @@
 from qcodes.instrument.base import Instrument as QcodesInstrument
-
+from qcodes.instrument.channel import InstrumentModule as QcodesInstrumentModule
 
 class Instrument(QcodesInstrument):
     """
@@ -11,4 +11,14 @@ class Instrument(QcodesInstrument):
         Required as a standard interface for QCoDeS instruments.
         """
         return {'driver': str(self.__class__), 'name': self.name}
-    
+
+class InstrumentModule(QcodesInstrumentModule):
+    """
+    Class for all QCodes instruments.
+    """
+
+    def get_idn(self):
+        """
+        Required as a standard interface for QCoDeS instruments.
+        """
+        return {'driver': str(self.__class__), 'name': self.name}
