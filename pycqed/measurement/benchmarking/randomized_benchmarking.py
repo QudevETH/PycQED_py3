462ef71d07202d12d76ca54b8c15c838cb76363b
Stefania Lazar <stefania.lazar@phys.ethz.ch>
2022-10-27 15:59:12 +0200

    SingleQubitXEB: bugfix in xeb_block related to using init_rotation

--- pycqed/measurement/benchmarking/randomized_benchmarking.py
+++ pycqed/measurement/benchmarking/randomized_benchmarking.py
@@ -683,9 +683,10 @@ class SingleQubitXEB(CrossEntropyBenchmarking):
                 'values', self.sweep_type['seqs'], 'z_rots')[seq_idx][nrcyc_idx]
             l = [['Y90', f'Z{zang}'] for zang in z_angles]
             # flatten l, prepend init pulse, append to pulse_op_codes_list
-            if self.init_rotation is not None:
-                pulse_op_codes_list += [[self.init_rotation]]
-            pulse_op_codes_list += [[e1 for e2 in l for e1 in e2]]
+            pulse_l_tmp = [] if self.init_rotation is None \
+                else [self.init_rotation]
+            pulse_l_tmp += [e1 for e2 in l for e1 in e2]
+            pulse_op_codes_list += [pulse_l_tmp]
 
         rb_block_list = [self.block_from_ops(
             f"rb_{task['qb']}", [f"{p} {task['qb']}" for p in
