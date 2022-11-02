6af33bf37995e07dafbcfd11dac635d873dd4d2e
Colin Scarato <cscarato@phys.ethz.ch>
2022-11-02 19:14:23 +0100

    MultiTWPA_SNR_Analysis: add prepare_plots

--- pycqed/analysis_v2/timedomain_analysis.py
+++ pycqed/analysis_v2/timedomain_analysis.py
@@ -10826,12 +10826,6 @@ class NDim_BaseDataAnalysis(ba.BaseDataAnalysis):
         sweep_points_ndim.update(task['ndim_sweep_points'])
         return sweep_points_ndim
 
-    def plot(self, *args, **kwargs):
-        # FIXME should we do an automatic plotting at all for N-dim data?
-        #  Note that the separate QuantumExperiments can all get their own
-        #  2-D data plotted using their own analyses
-        self.plot_slice()
-
     def plot_slice(self, slicing_plotting_list=None, **kw):
         """
         Generic method to plot a 2-dim slice of an N-dim dataset from
@@ -10938,3 +10932,146 @@ class MultiTWPA_SNR_Analysis(NDim_BaseDataAnalysis):
             pdd['SNR_rise'][mobjn] = pdd['gain_on'][mobjn] - pdd['gain_off'][
                 mobjn] - (pdd['noise_on'][mobjn] - pdd['noise_off'][mobjn])
         self.save_processed_data()
+
+    def prepare_plots(self):
+        """
+        Prepares plotting dicts for a gain/SNR comparison
+
+        In addition to gain and SNR measurements, this function can add vertical
+        lines on all plots at ro_freqs, which can be passed in self.options_dict
+        as a dict of the form {TWPA.name: [f1, f2, ...]}. Alternatively,
+        these can be extracted automatically from entries 'TWPAs' and
+        'qubits' if specified in self.options_dict.
+        """
+        pdd = self.proc_data_dict
+
+        for mobj_id in range(len(self.mobj_names) // 2):
+            mobjn = self.mobj_names[2 * mobj_id]  # FIXME see main FIXME
+
+            # Will draw vertical lines at these values
+            ro_freqs = self.options_dict.get('ro_freqs', [])
+            if not ro_freqs:
+                # An alternative way to define ro_freqs to plot is to pass
+                # TWPAs and qubits in the options_dict
+                if 'TWPAs' in self.options_dict:
+                    TWPA = [TWPA for TWPA in self.options_dict['TWPAs']
+                            if TWPA.name==mobjn]
+                    assert len(TWPA)==1, f"Please pass {mobjn} if providing " \
+                                         f"TWPAs in the options_dict"
+                    TWPA = TWPA[0]
+                    ro_freqs = [qb.ro_freq()
+                                for qb in self.options_dict.get('qubits', [])
+                                if qb.acq_unit() == TWPA.acq_unit()]
+
+            sp = pdd['sweep_points'][mobjn]
+            plotsize = self.get_default_plot_params(set=False)['figure.figsize']
+            gain_min = self.options_dict.get('gain_min', 0)
+            gain_max = self.options_dict.get('gain_max', 35)
+            snr_min = self.options_dict.get('snr_min', 0)
+            snr_max = self.options_dict.get('snr_max', 25)
+            fig_key = f"{mobjn}_gain_SNR"
+            gridspec_kw = {'height_ratios': [0.2] + [1]*len(sp['pump_freq'])}
+
+            for idx_freq, pump_freq in enumerate(sp['pump_freq']):
+                x = sp['freq'] / 1e9
+                y = sp['pump_power']
+                Z1, _ = sp_mod.SweepPoints.get_slice(
+                    pdd['gain_on'][mobjn] - pdd['gain_off'][mobjn],
+                    sp,
+                    {'pump_freq': pump_freq},
+                )
+                Z2, _ = sp_mod.SweepPoints.get_slice(
+                    pdd['SNR_rise'][mobjn],
+                    sp,
+                    {'pump_freq': pump_freq},
+                )
+
+                plot_key = f"{mobjn}_{pump_freq}"
+                # Gain data
+                self.plot_dicts[plot_key + "_gain"] = {
+                    'plotsize': (plotsize[0] * 2, len(sp['pump_freq'])*2+2),
+                    'gridspec_kw': gridspec_kw,
+                    'numplotsx': 2,
+                    'numplotsy': len(sp['pump_freq']) + 1,
+                    'sharex': False,
+                    'sharey': False,
+                    'ax_id': 2 + 2 * idx_freq,
+                    'fig_id': fig_key,
+                    'plotfn': self.plot_colorxy,
+                    'xvals': x,
+                    'yvals': y,
+                    'zvals': Z1.T,
+                    'zrange': (gain_min, gain_max),
+                    'xlabel': '',
+                    'xtick_labels': [],
+                    'xunit': '',
+                    'xlabels_rotation': 0,
+                    'ylabel': 'Pump\npwr.',
+                    'yunit': 'dBm',
+                    'plotcbar': False,
+                    'cmap': 'CMRmap',
+                    'title': f'Pump frequency {pump_freq / 1e9:.3f} GHz',
+                }
+                # SNR data
+                self.plot_dicts[plot_key + "_SNR"] = {
+                    'ax_id':  2 + 2 * idx_freq + 1,
+                    'fig_id': fig_key,
+                    'plotfn': self.plot_colorxy,
+                    'xvals': x,
+                    'yvals': y,
+                    'zvals': Z2.T,
+                    'zrange': (snr_min, snr_max),
+                    'xlabel': '',
+                    'xtick_labels': [],
+                    'xunit': '',
+                    'xlabels_rotation': 0,
+                    'ylabel': '',
+                    'ytick_labels': [],
+                    'yunit': '',
+                    'plotcbar': False,
+                    'cmap': 'CMRmap',
+                    # 'title': '',
+                }
+                # Colorbar
+                if idx_freq == len(sp['pump_freq']) - 1:
+                    self.plot_dicts[plot_key + "_gain"].update({
+                        'plotcbar': True,
+                        'cmap_levels': 100,
+                        'clabel': 'Signal gain, $G$ (dB)',
+                        'cax_id': 0,
+                        'orientation': 'horizontal',
+                        # 'location': 'top',
+                        'xlabel': 'Signal frequency',
+                        'xunit': 'GHz',
+                    })
+                    self.plot_dicts[plot_key + "_SNR"].update({
+                        'plotcbar': True,
+                        'cmap_levels': 100,
+                        'clabel': 'SNR gain, $G_{SNR}$ (dB)',
+                        'cax_id': 1,
+                        'orientation': 'horizontal',
+                        # 'location': 'top',
+                        'xlabel': 'Signal frequency',
+                        'xunit': 'GHz',
+                    })
+                # RO freqs
+                self.plot_dicts[plot_key + "_gain_ro_freqs"] = {
+                    'ax_id': 2 + 2 * idx_freq + 0,
+                    'fig_id': fig_key,
+                    'plotfn': self.plot_vlines,
+                    'x': np.array(ro_freqs)/1e9,
+                    'ymin': np.min(y),
+                    'ymax': np.max(y),
+                    'colors': 'white',
+                    'linestyles': '-',
+                }
+                self.plot_dicts[plot_key + "_SNR_ro_freqs"] = {
+                    'ax_id': 2 + 2 * idx_freq + 1,
+                    'fig_id': fig_key,
+                    'plotfn': self.plot_vlines,
+                    'x': np.array(ro_freqs)/1e9,
+                    'ymin': np.min(y),
+                    'ymax': np.max(y),
+                    'colors': 'white',
+                    'linestyles': '-',
+                }
