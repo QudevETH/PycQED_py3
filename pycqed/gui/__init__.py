0655b4bd1131e91b21834363848782a72d77ea52
mgiedraitis <mgiedraitis@student.ethz.ch>
2022-12-01 11:20:52 +0100

    Fixes from comments

--- pycqed/gui/__init__.py
+++ pycqed/gui/__init__.py
@@ -1,5 +1,5 @@
 # This import ensures that qt_compat is run everytime anything from the gui
-# package is imported. Ant qt_compat itself ensures that gui works regardless
+# package is imported. And qt_compat itself ensures that gui works regardless
 # whether it uses PyQt or PySide (there are some method naming differences
 # which are fixed with qt_compat)
 from pycqed.gui import qt_compat
