fcab522af40c7de2f48fd1af6d265c48f22f00f8
Colin Scarato <cscarato@phys.ethz.ch>
2023-02-02 17:00:49 +0100

    Mobj: move configure_offsets

--- pycqed/instrument_drivers/meta_instrument/MeasurementObject.py
+++ pycqed/instrument_drivers/meta_instrument/MeasurementObject.py
@@ -606,6 +606,27 @@ class MeasurementObject(Instrument):
         # set offsets and turn on AWG outputs
         self.configure_offsets()
 
+    def configure_offsets(self, set_ro_offsets=True, offset_list=None):
+        """
+        Set AWG channel DC offsets and switch sigouts on.
+
+        :param set_ro_offsets: whether to set offsets for RO channels
+        :param offset_list: additional offsets to set
+        """
+        pulsar = self.instr_pulsar.get_instr()
+        if offset_list is None:
+            offset_list = []
+
+        if set_ro_offsets:
+            offset_list += [('ro_I_channel', 'ro_I_offset'),
+                            ('ro_Q_channel', 'ro_Q_offset')]
+
+        for channel_par, offset_par in offset_list:
+            ch = self.get(channel_par)
+            if ch is not None and ch + '_offset' in pulsar.parameters:
+                pulsar.set(ch + '_offset', self.get(offset_par))
+                pulsar.sigout_on(ch)
+
     def link_param_to_operation(self, operation_name, parameter_name,
                                 argument_name):
         """
@@ -633,24 +654,3 @@ class MeasurementObject(Instrument):
         else:
             raise KeyError('Unknown operation {}, add '.format(operation_name) +
                            'first using add operation')
-
-    def configure_offsets(self, set_ro_offsets=True, offset_list=None):
-        """
-        Set AWG channel DC offsets and switch sigouts on.
-
-        :param set_ro_offsets: whether to set offsets for RO channels
-        :param offset_list: additional offsets to set
-        """
-        pulsar = self.instr_pulsar.get_instr()
-        if offset_list is None:
-            offset_list = []
-
-        if set_ro_offsets:
-            offset_list += [('ro_I_channel', 'ro_I_offset'),
-                            ('ro_Q_channel', 'ro_Q_offset')]
-
-        for channel_par, offset_par in offset_list:
-            ch = self.get(channel_par)
-            if ch is not None and ch + '_offset' in pulsar.parameters:
-                pulsar.set(ch + '_offset', self.get(offset_par))
-                pulsar.sigout_on(ch)
