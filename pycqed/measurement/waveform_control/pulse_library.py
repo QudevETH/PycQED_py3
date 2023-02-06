8beb08cca93643330ff97b0eda4ed674333af043
Kilian Hanke <hankek@student.ethz.ch>
2023-02-06 19:25:56 +0100

    CZ_nztc: bugfix fixed_pulse_length feature
    Tested on setup.
    
    (cherry picked from commit 77b71b11507917b09a8e82f716202d617fa248e8)

--- pycqed/measurement/waveform_control/pulse_library.py
+++ pycqed/measurement/waveform_control/pulse_library.py
@@ -498,11 +498,11 @@ class NZTransitionControlledPulse(GaussianFilteredPiecewiseConstPulse):
             self.amplitudes.append([0, ma + ao, ia, ta, -ta, -ia, -ma + ao, 0])
             self.lengths.append([bs + d - cl0, cl0 + ml / 2, il, tl / 2,
                                  tl / 2, il, ml / 2 + cl1, be - d - cl1])
-        if self.fixed_pulse_length is not None:
-            current_length = np.sum(self.lengths[0])
-            difference = self.fixed_pulse_length - current_length
-            self.lengths[0][0] += difference / 2
-            self.lengths[0][-1] += difference / 2
+            if self.fixed_pulse_length is not None:
+                current_length = np.sum(self.lengths[-1])
+                difference = self.fixed_pulse_length - current_length
+                self.lengths[-1][0] += difference / 2
+                self.lengths[-1][-1] += difference / 2
         while len(self.lengths) < len(self.channels):
             self.lengths += [[]]
         while len(self.amplitudes) < len(self.channels):
