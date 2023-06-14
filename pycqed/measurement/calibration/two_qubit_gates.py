97e2b3a8ebf5ea6cd31e944b7e45795d04466f3c
Colin Scarato <cscarato@phys.ethz.ch>
2023-06-14 10:57:03 +0200

    CPhase: improve docstring

--- pycqed/measurement/calibration/two_qubit_gates.py
+++ pycqed/measurement/calibration/two_qubit_gates.py
@@ -891,8 +891,9 @@ class CPhase(CalibBuilder):
             determined automatically
         :param prepend_pulse_dicts: (dict) prepended pulses, see
             CircuitBuilder.block_from_pulse_dicts
-        :param cphase: (float) conditional phase of the CZ gate (if already
-            calibrated)
+        :param cphase: (float) conditional phase of the CZ gate (relies on
+            the conditional phase being calibrated as a function of waveform
+            parameters)
         :param kw: further keyword arguments:
             cz_pulse_name: task-specific prefix of CZ gates (overwrites
                 global choice passed to the class init)
