c66f083dd617c71a7b339db488467bf93709bc57
Anatoly <akulikov@phys.ethz.ch>
2023-06-29 16:16:18 +0200

    Increasing the limit of single shot acquisitions per measurement

--- pycqed/instrument_drivers/meta_instrument/MeasurementObject.py
+++ pycqed/instrument_drivers/meta_instrument/MeasurementObject.py
@@ -62,7 +62,7 @@ class MeasurementObject(Instrument):
         self.add_parameter('acq_shots', initial_value=4094,
                            docstring='Number of single shot measurements to do'
                                      'in single shot experiments.',
-                           vals=vals.Ints(0, 1048576),
+                           vals=vals.Ints(0, 1048576**2),
                            parameter_class=ManualParameter)
         self.add_parameter('acq_length', initial_value=2.2e-6,
                            vals=vals.Numbers(min_value=1e-8,
