5370af69adfcfc79cdfffc1cd917a4180fc429b9
Yongxin Song <yosong@student.ethz.ch>
2023-09-05 17:26:13 +0200

    hdawg8_pulsar: use cached self.pulsar.clock() to not slow down waveform generation

--- pycqed/measurement/waveform_control/pulsar/hdawg8_pulsar.py
+++ pycqed/measurement/waveform_control/pulsar/hdawg8_pulsar.py
@@ -81,7 +81,7 @@ class HDAWG8Pulsar(PulsarAWGInterface, ZIPulsarMixin):
     def __init__(self, pulsar, awg):
         # Store clock at init to warn users changing clocks after the init,
         # which is currently not supported
-        self._clock_at_init = awg.clock_freq()
+        self._clock_at_init = pulsar.clock(awg=awg.name)
 
         super().__init__(pulsar, awg)
 
@@ -567,7 +567,7 @@ class HDAWG8Pulsar(PulsarAWGInterface, ZIPulsarMixin):
                     if self.awg._awg_program[awg_nr] is not None])
 
     def clock(self):
-        clock = self.awg.clock_freq()
+        clock = self.pulsar.clock(awg=self.awg.name)
         if clock != self._clock_at_init:
             raise NotImplementedError('HDAWG8Pulsar: detected a change of ' +
                                       'sampling rate from ' +
