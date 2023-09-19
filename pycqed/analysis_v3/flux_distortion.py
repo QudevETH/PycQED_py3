5cbf2f3e74b56d331b5aa0d6cd3b5d31f91a4214
Colin Scarato <cscarato@phys.ethz.ch>
2023-09-19 17:46:15 +0200

    fit_exp_lmfit: remove print statement

--- pycqed/analysis_v3/flux_distortion.py
+++ pycqed/analysis_v3/flux_distortion.py
@@ -887,7 +887,6 @@ def fit_exp_lmfit(x, y, start_vals=None, fixed_A=None, weights=None, **kw):
         fixed_vals = kw.pop('fixed_vals')
         for par_name in fixed_vals:
             lmfit_model.set_param_hint(par_name, vary=False)
-    # print(kw)
     res = lmfit_model.fit(y, x=x, weights=weights, **kw, **fit_params)
     return res
 
