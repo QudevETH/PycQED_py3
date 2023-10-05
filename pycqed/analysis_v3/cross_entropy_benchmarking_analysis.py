267e4827019385fc87f7be0eee817465e0d86996
Colin Scarato <cscarato@phys.ethz.ch>
2023-10-05 14:55:24 +0200

    xeb_ana: add fixme

--- pycqed/analysis_v3/cross_entropy_benchmarking_analysis.py
+++ pycqed/analysis_v3/cross_entropy_benchmarking_analysis.py
@@ -1100,6 +1100,10 @@ def calculate_cz_error(data_dict1, data_dict2, **params):
     type = params.get('error_type', 'pauli')
 
     try:
+        # FIXME This try except is used since the fits saved in the data dict
+        #  have slightly different formats if the data has been extracted
+        #  from a previous analysis run saved in an hdf file. This should be
+        #  fixed in the data extraction instead.
         e1_1 = data_dict1[meas_obj_names[0]][f'fit_res_{metric}'].best_values['e']
         e1_2 = data_dict1[meas_obj_names[1]][f'fit_res_{metric}'].best_values['e']
         e1_1e = data_dict1[meas_obj_names[0]][f'fit_res_{metric}'].params[
