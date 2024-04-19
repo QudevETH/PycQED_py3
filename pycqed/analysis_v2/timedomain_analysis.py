tda: sign corrections after f1482ef7b41138903612395cccba7d54f6c63af0 correct sign in MultiCZgate_Calib_Analysis add legacy_sign to allow re-analysing old measurements

pycqed/analysis_v2/timedomain_analysis.py
@@ -5745,10 +5745,12 @@ class RamseyAnalysis(MultiQubit_TimeDomain_Analysis, ArtificialDetuningMixin):
                 old_qb_freq = 0  # FIXME: explain why
             self.proc_data_dict['analysis_params_dict'][outer_key][fit_type][
                 'old_qb_freq'] = old_qb_freq
+            legacy_sign = 1 if self.metadata.get('right_handed_basis')\
+                else -1  # -1 for old measurements with left-handed basis
             self.proc_data_dict['analysis_params_dict'][outer_key][fit_type][
-                'new_qb_freq'] = old_qb_freq \
-                                 + fit_res.best_values['frequency'] \
-                                 - self.artificial_detuning_dict[qbn]
+                'new_qb_freq'] = old_qb_freq + legacy_sign * (
+                    fit_res.best_values['frequency']
+                    - self.artificial_detuning_dict[qbn])
             self.proc_data_dict['analysis_params_dict'][outer_key][fit_type][
                 'new_qb_freq_stderr'] = fit_res.params['frequency'].stderr
             self.proc_data_dict['analysis_params_dict'][outer_key][fit_type][
@@ -7322,10 +7324,13 @@ class MultiCZgate_Calib_Analysis(MultiQubit_TimeDomain_Analysis):
                         'val': phases, 'stderr': phases_errs}
 
                     # compute phase diffs
+                    # this can be false for Cryoscope with
+                    # estimation_window == None and odd nr of trunc lengths
                     if getattr(self, 'delta_tau', 0) is not None:
-                        # this can be false for Cyroscope with
-                        # estimation_window == None and odd nr of trunc lengths
-                        phase_diffs = phases[0::2] - phases[1::2]
+                        # -1 for old measurements with left-handed basis
+                        legacy_sign = 1 if self.metadata.get(
+                            'right_handed_basis') else -1
+                        phase_diffs = legacy_sign*(phases[1::2] - phases[0::2])
                         phase_diffs %= (2*np.pi)
                         phase_diffs_stderrs = np.sqrt(
                             np.array(phases_errs[0::2]**2 +
@@ -7721,7 +7726,9 @@ class CryoscopeAnalysis(DynamicPhaseAnalysis):
                 delta_tau = delta_tau[m]
                 phases = self.proc_data_dict['analysis_params_dict'][
                     f'phases_{qbn}']
-                delta_phases_vals = np.diff(phases['val'])[m]
+                legacy_sign = 1 if self.metadata.get('right_handed_basis')\
+                    else -1  # -1 for old measurements with left-handed basis
+                delta_phases_vals = legacy_sign * np.diff(phases['val'])[m]
                 delta_phases_vals = (delta_phases_vals + np.pi) % (
                             2 * np.pi) - np.pi
                 delta_phases_errs = (np.sqrt(