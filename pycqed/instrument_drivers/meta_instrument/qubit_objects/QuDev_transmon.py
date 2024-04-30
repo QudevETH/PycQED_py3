18e6d24cd59c764414f870eeace3dd3f2520c55c
Graham J. Norris <graham.norris@phys.ethz.ch>
2024-04-30 10:43:21 +0200

    qudev transmon: remove two-step readout pulse
    Remove GaussFilteredCosIQPulseTwoStep from the ro pulse type validator
    since this no longer exists (it was generalized into multiple steps and
    integrated into the base GaussFilteredCosIQPulse).

--- pycqed/instrument_drivers/meta_instrument/qubit_objects/QuDev_transmon.py
+++ pycqed/instrument_drivers/meta_instrument/qubit_objects/QuDev_transmon.py
@@ -60,7 +60,6 @@ class QuDev_transmon(MeasurementObject):
     }
     _ro_pulse_type_vals = ['GaussFilteredCosIQPulse',
                            'GaussFilteredCosIQPulseMultiChromatic',
-                           'GaussFilteredCosIQPulseTwoStep',
                            'GaussFilteredCosIQPulseWithFlux']
     _allowed_drive_modes = [None, 'continuous_spec',
                             'continuous_spec_modulated', 'pulsed_spec',
