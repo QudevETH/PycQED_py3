e343a390b1867d28de508a1df286452b143d2cb5
Jakob Ekert <jakob.ekert@phys.ethz.ch>
2024-06-03 17:54:40 +0200

    a_tools.compare_instrument_settings: updated warning message - explains how to use settings manager for comparison of timestamps

--- pycqed/analysis/analysis_toolbox.py
+++ pycqed/analysis/analysis_toolbox.py
@@ -532,9 +532,12 @@ def compare_instrument_settings(a, b, folder=None, instruments='all',
         None or the results as str or dict, see parameter of arg output
     """
     log.warning('This function is no longer maintained and works only for hdf'
-                'files. Please use the settings manager '
-                '(pycqed.utilitities.settings_manager.SettingsManager) and the'
-                'function compare_stations().')
+                'files. '
+                'Please instantiate a settings manager object '
+                '(pycqed.utilitities.settings_manager.SettingsManager) and '
+                'use the function '
+                'SettingsManager.compare_stations([%ts1%, %ts2%, %ts3%, ...]) '
+                'instead.')
     h5mode = 'r'
     files_to_close = []
     try:
