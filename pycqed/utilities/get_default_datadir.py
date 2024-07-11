d6014956627e0e91bc59188a32147173cac3fa01
Roland Siegbert <roland.siegbert@phys.ethz.ch>
2024-07-11 15:30:40 +0200

    Increase robustness of get_default_datadir()
    Depending upon setup the convention for the `data` location might not be
    set in the assumed way.

--- pycqed/utilities/get_default_datadir.py
+++ pycqed/utilities/get_default_datadir.py
@@ -5,10 +5,36 @@ import pycqed
 
 def get_default_datadir():
     """
-    Returns the default datadir in the repostory folder.
+    Returns the default datadir in the repository folder.
+
+    This function attempts to locate the default data directory for PyCQED.
+    It first checks if the data directory exists in the PyCQED installation folder.
+    If not found, it searches for a 'data' directory in the current working directory
+    and its parent directories (up to two levels deep).
+
+    Returns:
+        str: The path to the default data directory.
+
+    Note:
+        The default data location is typically 'pycqed_py3/data/'.
     """
-    # Stores data in the default data location (pycqed_py3/data/)
-    datadir = os.path.abspath(os.path.join(os.path.dirname(pycqed.__file__),
-                                           os.pardir, 'data'))
-    logging.info('Setting datadir to default location: {}'.format(datadir))
+
+    # Depending upon how pcyqed was installed, pycqed.__file__ can evaluate to None
+    if pycqed.__file__ is not None:
+        datadir = os.path.abspath(os.path.join(os.path.dirname(pycqed.__file__), os.pardir, 'data'))
+    else:
+        # Search the directory tree from which this script was called 
+        # for a depth of two directories for a directory called "data".
+        current_dir = os.getcwd()
+        for root, dirs, files in os.walk(current_dir):
+            if 'data' in dirs:
+                return os.path.join(root, 'data')
+            if root.count(os.sep) - current_dir.count(os.sep) == 2:
+                break
+    
+    # If no 'data' directory is found, return a default path
+    datadir = os.path.join(current_dir, 'data')
+
+    # Show the result
+    logging.warn('Setting datadir to location: {}'.format(datadir))
     return datadir
