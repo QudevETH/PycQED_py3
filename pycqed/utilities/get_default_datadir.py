183c3f623057bb8fc9a2ee27af400a8d222a90fe
Roland Siegbert <roland.siegbert@phys.ethz.ch>
2024-07-11 15:54:34 +0200

    Fix unbound local error in get_default_datadir

--- pycqed/utilities/get_default_datadir.py
+++ pycqed/utilities/get_default_datadir.py
@@ -18,6 +18,7 @@ def get_default_datadir():
     Note:
         The default data location is typically 'pycqed_py3/data/'.
     """
+    current_dir = os.getcwd()
 
     # Depending upon how pcyqed was installed, pycqed.__file__ can evaluate to None
     if pycqed.__file__ is not None:
@@ -25,7 +26,6 @@ def get_default_datadir():
     else:
         # Search the directory tree from which this script was called 
         # for a depth of two directories for a directory called "data".
-        current_dir = os.getcwd()
         for root, dirs, files in os.walk(current_dir):
             if 'data' in dirs:
                 return os.path.join(root, 'data')
