fade73bc033dd2a4777d9263f52f15570a93573e
Luca Hofele <lhofele@phys.ethz.ch>
2024-07-17 11:38:07 +0200

    Make pulse_length a qcodes parameter
    - Note that this causes the change that changing the pulse_length now does not run program_awg() anymore.

--- pycqed/instrument_drivers/meta_instrument/HDAWG_TriggerDevice.py
+++ pycqed/instrument_drivers/meta_instrument/HDAWG_TriggerDevice.py
@@ -36,7 +36,17 @@ class HDAWG_TriggerDevice(Instrument):
         super().__init__(name=name)
         self.awg = awg
         self._awg_nr = None  # to avoid that the next line programs the AWG
-        self.pulse_length = self.PULSE_LENGTH
+        # add pulse_length parameter, check that it is a multiple of the
+        # waveform granularity up to 1e-12.
+        self.add_parameter(
+            'pulse_length',
+            label='Pulse length',
+            get_cmd=(lambda self=self: self.PULSE_LENGTH),
+            set_cmd=(lambda val, self=self: setattr(self, 'PULSE_LENGTH',
+                                                    val)),
+            unit='samples',
+            vals=vals.PermissiveMultiples(self.GRANULARITY, 1e-12)
+        )
         self.awg_nr = awg_nr  # this programs the AWG
 
         self.add_parameter(
@@ -65,26 +75,6 @@ class HDAWG_TriggerDevice(Instrument):
             vals=vals.Ints(1, int(1e15)))
 
     @property
-    def pulse_length(self):
-        return self._pulse_length
-
-    @pulse_length.setter
-    def pulse_length(self, value):
-        """
-        Set the pulse length in samples. The pulse length must be a multiple
-        of the waveform granularity.
-
-        :param value: the pulse length in samples
-        """
-        # TODO this could become a qcodes parameter in unit of seconds
-        if np.abs(value % self.GRANULARITY) > 1e-12:
-            raise ValueError(
-                f'Pulse length {value} samples is not a multiple of the '
-                f'waveform granularity {self.GRANULARITY} samples.')
-        self._pulse_length = value
-        self.program_awg()
-
-    @property
     def awg_nr(self):
         return self._awg_nr
 
@@ -103,7 +93,7 @@ class HDAWG_TriggerDevice(Instrument):
         awg_str = self.TEMPLATE.format(
             reps=self.USER_REG_REPETITIONS,
             sep=self.USER_REG_SEPARATION,
-            pulse_length=self.pulse_length
+            pulse_length=self.pulse_length()
         )
         self.awg.configure_awg_from_string(awg_nr=self.awg_nr,
                                            program_string=awg_str)
@@ -121,7 +111,7 @@ class HDAWG_TriggerDevice(Instrument):
                 f'Pulse period {pulse_period}s is not a multiple of the '
                 f'waveform granularity {self.GRANULARITY} samples.')
         self.stop()
-        return samples - self.pulse_length
+        return samples - self.pulse_length()
 
     def _pulse_period_get_parser(self, pulse_distance):
         """
@@ -130,7 +120,7 @@ class HDAWG_TriggerDevice(Instrument):
 
         :param pulse_distance: the pulse distance in samples
         """
-        samples = pulse_distance + self.pulse_length
+        samples = pulse_distance + self.pulse_length()
         return samples / self.awg.clock_freq()
 
     def start(self, **kw):
