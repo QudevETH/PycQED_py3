e2bee5dab75d48b59c6fbb3837ad1c61649c6cac
Colin Scarato <cscarato@phys.ethz.ch>
2024-07-29 13:46:42 +0000

    Apply 1 suggestion(s) to 1 file(s)
    Co-authored-by: Luca Hofele <lhofele@ethz.ch>

--- pycqed/measurement/sweep_points.py
+++ pycqed/measurement/sweep_points.py
@@ -297,7 +297,7 @@ class SweepPoints(list):
          {mobj_name: [sweep_param_name_0, ..., sweep_param_name_n]}
         """
 
-        # Ensure measurement_objects is a list
+        # Ensure measurement_objects is a list or tuple
         if isinstance(measurement_objects, (list, tuple)):
             measurement_objects = copy(measurement_objects)
         else:
