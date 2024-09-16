73d7fbdae837a7f3449d7680562ff4bc25c71a40
Christoph Hellings <chellings@phys.ethz.ch>
2024-09-16 17:35:13 +0200

    ZI drivers: clean up generate_snapshot_whitelist_hdawg/uhfqa

--- pycqed/instrument_drivers/physical_instruments/ZurichInstruments/snapshot_whitelist.py
+++ pycqed/instrument_drivers/physical_instruments/ZurichInstruments/snapshot_whitelist.py
@@ -9,31 +9,29 @@ def generate_snapshot_whitelist_hdawg():
         'system_clocks_sampleclock_freq'}
     for i in range(4):
         snapshot_whitelist.update({
-            'awgs_{}_enable'.format(i),
-            'awgs_{}_outputs_0_amplitude'.format(i),
-            'awgs_{}_outputs_1_amplitude'.format(i)})
+            f'awgs_{i}_enable',
+            f'awgs_{i}_outputs_0_amplitude',
+            f'awgs_{i}_outputs_1_amplitude'})
     for i in range(8):
         snapshot_whitelist.update({
-            'sigouts_{}_direct'.format(i),
-            'sigouts_{}_offset'.format(i),
-            'sigouts_{}_on'.format(i),
-            'sigouts_{}_range'.format(i),
-            'sigouts_{}_delay'.format(i)})
+            f'sigouts_{i}_direct',
+            f'sigouts_{i}_offset',
+            f'sigouts_{i}_on',
+            f'sigouts_{i}_range',
+            f'sigouts_{i}_delay'})
     return snapshot_whitelist
 
 def generate_snapshot_whitelist_uhfqa():
     """Returns the default snapshot white list for ZI UHFQA instruments"""
     snapshot_whitelist = {
         'IDN',
-        'clockbase', }
-    for i in range(1):
-        snapshot_whitelist.update({
-            'awgs_{}_enable'.format(i),
-            'awgs_{}_outputs_0_amplitude'.format(i),
-            'awgs_{}_outputs_1_amplitude'.format(i)})
+        'clockbase',
+        'awgs_0_enable',
+        'awgs_0_outputs_0_amplitude',
+        'awgs_0_outputs_1_amplitude'}
     for i in range(2):
         snapshot_whitelist.update({
-            'sigouts_{}_offset'.format(i),
-            'sigouts_{}_on'.format(i),
-            'sigouts_{}_range'.format(i), })
+            f'sigouts_{i}_offset',
+            f'sigouts_{i}_on',
+            f'sigouts_{i}_range',})
     return snapshot_whitelist
