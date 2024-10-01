883c1f0d83df2fe88720e29b877b6540e5657c52
Roland Siegbert <roland.siegbert@phys.ethz.ch>
2024-10-01 12:24:34 +0200

    Add module name to test_analysis_toolbox for easier retrieval in GitLab

--- pycqed/tests/general_functionality/test_analysis_toolbox.py
+++ pycqed/tests/general_functionality/test_analysis_toolbox.py
@@ -13,7 +13,7 @@ def setup_datadir():
         yield tmpdirname
         a_tools.datadir = old_datadir
 
-def test_get_folder_happy_path(setup_datadir):
+def test_atools_get_folder_happy_path(setup_datadir):
     """Create a mock data folder and run happy test."""
     timestamp = '20230101_120000'
     os.makedirs(os.path.join(setup_datadir, '20230101', '120000_test_measurement'))
@@ -21,7 +21,7 @@ def test_get_folder_happy_path(setup_datadir):
     folder = a_tools.get_folder(timestamp)
     assert folder == os.path.join(setup_datadir, '20230101', '120000_test_measurement')
 
-def test_get_folder_bad_path():
+def test_atools_get_folder_bad_path():
     """Set datadir to None to simulate the bad path."""
     old_datadir = a_tools.datadir
     a_tools.datadir = None
