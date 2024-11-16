26e53f7b8398b9c0360050824e4ea16537cb6443
Graham J. Norris <graham.norris@phys.ethz.ch>
2024-11-16 21:20:28 +0100

    calib/acr: refactor split_step_for_parallel_groups
    plus dropped qubit detection into a single function to reduce code
    duplication.

--- pycqed/measurement/calibration/automatic_calibration_routines/initial_qubit_parking.py
+++ pycqed/measurement/calibration/automatic_calibration_routines/initial_qubit_parking.py
@@ -521,14 +521,7 @@ class InitialQubitParking(AutomaticCalibrationRoutine):
     def create_routine_template(self):
         """Creates routine template."""
         super().create_routine_template()
-        # Cache original step labels for later error check
-        step_labels = [step[1] for step in self.routine_template]
-        # Loop in reverse order so that the correspondence between the index
-        # of the loop and the index of the routine_template steps is preserved
-        # when new steps are added
-        for i, step in reversed(list(enumerate(self.routine_template))):
-            self.split_step_for_parallel_groups(index=i)
-        super().check_for_dropped_qubits(step_labels)
+        super().split_routine_template_for_parallel_groups()
 
     _DEFAULT_ROUTINE_TEMPLATE = RoutineTemplate([
         [FeedlineSpectroscopyStep, 'feedline_spectroscopy', {}],
--- pycqed/measurement/calibration/automatic_calibration_routines/single_qubit_routines.py
+++ pycqed/measurement/calibration/automatic_calibration_routines/single_qubit_routines.py
@@ -644,14 +644,7 @@ class PiPulseCalibration(AutomaticCalibrationRoutine):
         """Creates routine template.
         """
         super().create_routine_template()
-        # Cache original step labels for later error check
-        step_labels = [step[1] for step in self.routine_template]
-        # Loop in reverse order so that the correspondence between the index
-        # of the loop and the index of the routine_template steps is preserved
-        # when new steps are added
-        for i, step in reversed(list(enumerate(self.routine_template))):
-            self.split_step_for_parallel_groups(index=i)
-        super().check_for_dropped_qubits(step_labels)
+        super().split_routine_template_for_parallel_groups()
 
     _DEFAULT_ROUTINE_TEMPLATE = RoutineTemplate([
         [RabiStep, 'rabi', {}],
@@ -1129,15 +1122,8 @@ class SingleQubitCalib(AutomaticCalibrationRoutine):
                                                    step_tmp_settings)
 
         self.routine_template = detailed_routine_template
+        super().split_routine_template_for_parallel_groups()
 
-        # Cache original step labels for later error check
-        step_labels = [step[1] for step in self.routine_template]
-        # Loop in reverse order so that the correspondence between the index
-        # of the loop and the index of the routine_template steps is preserved
-        # when new steps are added
-        for i, step in reversed(list(enumerate(self.routine_template))):
-            self.split_step_for_parallel_groups(index=i)
-        super().check_for_dropped_qubits(step_labels)
 
     class SQCPreparation(IntermediateStep):
         """
