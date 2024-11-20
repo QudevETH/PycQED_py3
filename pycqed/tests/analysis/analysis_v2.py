0b4b53df53111bfbaa5d8624ec481389412a8ad3
Colin Scarato <cscarato@phys.ethz.ch>
2024-11-20 15:39:40 +0100

    tests anav2: add Chevron with XLD1 data

--- pycqed/tests/analysis/analysis_v2.py
+++ pycqed/tests/analysis/analysis_v2.py
@@ -9,6 +9,15 @@ def set_module_data_dir(test_data_base_dir):
     a_tools.datadir = str(sub_directory)
     return sub_directory
 
+def add_default_kw(kw=None):
+    if kw is None:
+        kw = {}
+    kw.setdefault('options_dict', {})
+    kw.setdefault('extract_only', False)
+    kw.setdefault('raise_exceptions', True)
+    kw['options_dict'].setdefault('delegate_plotting', False)
+    return kw
+
 def test_Rabi():
     for t_start in [
         '20241119_110543',  # avg
@@ -19,7 +28,62 @@ def test_Rabi():
         tda.RabiAnalysis(
             t_start=t_start,
             do_fitting=True,
-            options_dict={'delegate_plotting': False},
-                extract_only=True,
-                raise_exceptions=True,
+            **add_default_kw(),
+        )
+
+def test_Chevron():
+    for t_start, kw in [
+        # avg
+        ('20241108_012425',
+            {
+                'do_fitting': True,
+            }
+        ),
+        # SSRO
+        (
+            '20241107_212929',
+            {
+                'do_fitting': False,
+            }
+        ),
+        (
+            '20241107_212929',
+            {
+                'do_fitting': False,
+                'options_dict': {
+                    'predict_proba': True,
+                },
+            }
+        ),
+        (
+            '20241107_212929',
+            {
+                'do_fitting': False,
+                'options_dict': {
+                    'predict_proba': True,
+                    'thresholding': True,
+                },
+            }
+        ),
+        (
+            '20241107_212929',
+            {
+                'do_fitting': False,
+                'options_dict': {
+                    'predict_proba': True,
+                    'correlate_proba': True,
+                },
+            }
+        ),
+    ]:
+        add_default_kw(kw)
+        tda.ChevronAnalysis(
+            qb_names=['qb14', 'qb13'],
+            t_start=t_start,
+            **kw,
+            # options_dict={  # TODO could still vary these
+            #     'plot_raw_data': True,
+            #     'plot_proj_data': True,
+            #     'rotate': True,
+            # },
         )
