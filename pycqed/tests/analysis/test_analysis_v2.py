cc003e52712b88610cf9368086e9872edfb53619
Colin Scarato <cscarato@phys.ethz.ch>
2024-11-21 13:30:52 +0100

    tests: add tests from analysis demo notebook tomography analyses are still TODO

--- pycqed/tests/analysis/test_analysis_v2.py
+++ pycqed/tests/analysis/test_analysis_v2.py
@@ -18,12 +18,15 @@ def add_default_kw(kw=None):
     kw['options_dict'].setdefault('delegate_plotting', False)
     return kw
 
-def test_Rabi():
+def test_RabiAnalysis():
     for t_start in [
+        '20210716_171937',  # ge
+        '20210716_165406',  # ef
+        '20210716_002652',  # ef - multi-qubit
         '20241120_155114',  # avg
         '20241120_163229',  # avg with reset
         '20241120_155158',  # SSRO
-        '20241120_163256',  # SSRO with reset
+        # '20241120_163256',  # SSRO with reset
     ]:
         tda.RabiAnalysis(
             t_start=t_start,
@@ -31,12 +34,22 @@ def test_Rabi():
             **add_default_kw(),
         )
 
-def test_Chevron():
+def test_ChevronAnalysis():
     for t_start, kw in [
         # avg
         ('20241108_012425',
             {
                 'do_fitting': True,
+            }
+        ),
+        ('20241108_012425',
+            {
+                'options_dict': {
+                    'slice_idxs_1d_proj_plot': {
+                        'qb14': [('8:10', 'row'), (0, 'col')]},
+                    'slice_idxs_1d_raw_plot': {
+                        'qb14': [('8:10', 'row'), (0, 'col')]},
+                },
             }
         ),
         # SSRO
@@ -88,7 +101,7 @@ def test_Chevron():
             # },
         )
 
-def test_CPhase():
+def test_CPhaseLeakageAnalysis():
     for t_start, kw in [
         ('20241116_160921', {}),  # 1D
         ('20241116_155729', {}),  # 2D
@@ -100,13 +113,341 @@ def test_CPhase():
             **kw,
         )
 
-def test_QScale():
+def test_QScaleAnalysis():
     for t_start, kw in [
+        ('20210716_161150', {}),
         ('20241115_002806', {}),
     ]:
         add_default_kw(kw)
         tda.QScaleAnalysis(
-            qb_names=['qb5', 'qb10'],
             t_start=t_start,
             **kw,
         )
+
+def test_T1Analysis():
+    for t_start, kw in [
+        ('20210716_002204', {}),  # 1 qb
+        ('20210716_235757', {}),  # n qbs
+    ]:
+        add_default_kw(kw)
+        tda.T1Analysis(
+            t_start=t_start,
+            **kw,
+        )
+
+def test_RamseyAnalysis():
+    for t_start, kw in [
+        ('20210716_160058', {}),  # Ramsey ge - 1qb
+        ('20210716_174638', {}),  # Ramsey ef - 1qb
+        ('20210716_235343', {}),  # Ramsey ge - nqb
+        ('20211221_114300', {}),  # Ramsey ge with different artificial dets
+    ]:
+        add_default_kw(kw)
+        tda.RamseyAnalysis(
+            t_start=t_start,
+            **kw,
+        )
+
+def test_EchoAnalysis():
+    for t_start, kw in [
+        ('20220520_191131', {}),
+    ]:
+        add_default_kw(kw)
+        tda.EchoAnalysis(
+            t_start=t_start,
+            **kw,
+        )
+
+def test_DynamicPhaseAnalysis():
+    for t_start, kw in [
+        ('20210721_090259', {}),  # cphase check
+        ('20210721_062113', {}),  # cphase sweep
+        ('20210721_083238', {}),  # dyn phase
+    ]:
+        add_default_kw(kw)
+        tda.DynamicPhaseAnalysis(
+            t_start=t_start,
+            **kw,
+        )
+
+def test_MultiQubitTDA():
+    for t_start, kw in [
+        ('20210809_123011', {}),  # chevron
+    ]:
+        add_default_kw(kw)
+        tda.MultiQubit_TimeDomain_Analysis(
+            t_start=t_start,
+            **kw,
+        )
+
+def test_FluxPulseScopeAnalysis():
+    for t_start, kw in [
+        ('20210317_210331', {}),  # non-tracked
+        ('20211210_222258', {}),  # tracked
+    ]:
+        add_default_kw(kw)
+        tda.FluxPulseScopeAnalysis(
+            t_start=t_start,
+            **kw,
+        )
+
+def test_CryoscopeAnalysis():
+    for t_start, kw in [
+        ('20211219_081948', {}),
+    ]:
+        add_default_kw(kw)
+        tda.CryoscopeAnalysis(
+            t_start=t_start,
+            **kw,
+        )
+
+def test_RabiFrequencySweepAnalysis():
+    for t_start, kw in [
+        ('20211202_175810', {}),
+    ]:
+        add_default_kw(kw)
+        tda.RabiFrequencySweepAnalysis(
+            t_start=t_start,
+            **kw,
+        )
+
+def test_ReparkingRamseyAnalysis():
+    for t_start, kw in [
+        ('20220118_134248', {}),
+    ]:
+        add_default_kw(kw)
+        tda.ReparkingRamseyAnalysis(
+            t_start=t_start,
+            **kw,
+        )
+
+def test_T1FrequencySweepAnalysis():
+    for t_start, kw in [
+        # ('20210825_225159', {}),  # TODO r'Q:\Archive\Qudev86 - QComp PycQED data from BF1\pydata'
+    ]:
+        add_default_kw(kw)
+        tda.T1FrequencySweepAnalysis(
+            t_start=t_start,
+            **kw,
+        )
+
+def test_MixerCarrierAnalysis():
+    for t_start, kw in [  # TODO
+        # (
+        #     '20211116_113037',
+        #     {
+        #         'options_dict': {
+        #             'qb_names': ['qb4'],
+        #             'rotate': False,
+        #         },
+        #     }
+        # ),  # LO with random grid
+        # (
+        #     '20211127_152630',
+        #     {
+        #         'options_dict': {
+        #             'qb_names': ['qb4'],
+        #             'rotate': False,
+        #         },
+        #     }
+        # ),  # LO with rectangular grid
+    ]:
+        add_default_kw(kw)
+        tda.MixerCarrierAnalysis(
+            t_start=t_start,
+            **kw,
+        )
+
+def test_MixerSkewnessAnalysis():
+    for t_start, kw in [
+        ('20220317_182154', {}),
+    ]:
+        add_default_kw(kw)
+        tda.MixerSkewnessAnalysis(
+            t_start=t_start,
+            **kw,
+        )
+
+def test_FluxPulseTimingAnalysis():
+    for t_start, kw in [
+        (
+            '20211216_010723',
+            {
+                'qb_names': ['qb2'],
+            }
+        ),
+        # ('', {}),
+    ]:
+        add_default_kw(kw)
+        tda.FluxPulseTimingAnalysis(
+            t_start=t_start,
+            **kw,
+        )
+
+def test_FluxPulseTimingBetweenQubitsAnalysis():
+    for t_start, kw in [
+        (
+            '20211216_011402',
+            {
+                'options_dict': {
+                    'data_to_fit': {'qb2': 'pe', 'qb4': 'pe'}  # FIXME remove?
+                },
+            }
+        ),
+        # ('', {}),
+    ]:
+        add_default_kw(kw)
+        tda.FluxPulseTimingBetweenQubitsAnalysis(
+            t_start=t_start,
+            **kw,
+        )
+
+def test_FluxlineCrosstalkAnalysis():
+    for t_start, kw in [
+        ('20220202_024818', {}),  # multi qb
+        ('20220202_024222', {}),  # single qb
+    ]:
+        add_default_kw(kw)
+        # FIXME v2 shouldn't rely on v3. Imported only locally here
+        from pycqed.analysis_v3 import helper_functions as hlp_mod
+        crosstalk_qubits_names = hlp_mod.get_param_from_metadata_group(
+            t_start, 'crosstalk_qubits_names')
+        tda.FluxlineCrosstalkAnalysis(
+            qb_names=crosstalk_qubits_names,
+            t_start=t_start,
+            **kw,
+        )
+
+def test_MeasurementInducedDephasingAnalysis():
+    for t_start, kw in [
+        ('20221013_140524', {}),
+    ]:
+        add_default_kw(kw)
+        tda.MeasurementInducedDephasingAnalysis(
+            t_start=t_start,
+            **kw,
+        )
+
+def test_FluxAmplitudeSweepAnalysis():
+    for t_start, kw in [
+        ('20230828_170441', {}),
+    ]:
+        add_default_kw(kw)
+        # FIXME v2 shouldn't rely on v3. Imported only locally here
+        from pycqed.analysis_v3 import helper_functions as hlp_mod
+        ro_qubits = hlp_mod.get_param_from_metadata_group(t_start, 'ro_qubits')
+        tda.FluxAmplitudeSweepAnalysis(
+            qb_names=ro_qubits,
+            t_start=t_start,
+            **kw,
+        )
+
+def test_NPulseAmplitudeCalibAnalysis():
+    for t_start, kw in [
+        ('20220521_113929', {}),
+    ]:
+        add_default_kw(kw)
+        tda.NPulseAmplitudeCalibAnalysis(
+            t_start=t_start,
+            **kw,
+        )
+
+def test_DriveAmplitudeNonlinearityCurveAnalysis():
+    for ts, kw in [
+        (('20220515_164605', '20220515_175316'), {}),
+    ]:
+        add_default_kw(kw)
+        tda.DriveAmplitudeNonlinearityCurveAnalysis(
+            t_start=ts[0],
+            t_stop=ts[1],
+            **kw,
+        )
+
+# TODO State Tomography Analysis
+# ts = '20200422_215530'
+# gate = ('qb2', 'qb4')
+# import pycqed.utilities.qutip_compat as qt
+# qtp = qt
+# U1s = {pulse: tomo_ana.standard_qubit_pulses_to_rotations([(pulse,)])[0] for
+#        pulse in
+#        ['X90', 'Y90', 'mX90', 'mY90', 'I', 'X180', 'Y180', 'mY180', 'mX180']}
+# UCZ = qtp.Qobj(np.diag([1, 1, 1, -1]), dims=[[2, 2], [2, 2]])
+# cz_pulse_name = 'CZ'
+# state_dict = {
+#     'phi+': ([f'mY90 {gate[0]}', f'Y90s {gate[1]}',
+#               f'{cz_pulse_name} {gate[0]} {gate[1]}',
+#               f'mY90 {gate[1]}'],
+#              (qtp.tensor([U1s['I'], U1s['mY90']]) * UCZ * qtp.tensor(
+#                  [U1s['mY90'], U1s['Y90']]) *
+#               qtp.tensor(qtp.basis(2), qtp.basis(2))).full()),
+#     'mY90Y90': ([f'mY90 {gate[0]}', f'Y90s {gate[1]}'],
+#                 (qtp.tensor([U1s['mY90'], U1s['Y90']]) * qtp.tensor(
+#                     qtp.basis(2), qtp.basis(2))).full()),
+#     'CZ': ([f'{cz_pulse_name} {gate[0]} {gate[1]}'],
+#            (UCZ * qtp.tensor(qtp.basis(2), qtp.basis(2))).full()),
+#     'IX90CZ': ([f'X90 {gate[1]}', f'{cz_pulse_name} {gate[0]} {gate[1]}'],
+#                (UCZ * qtp.tensor([U1s['I'], U1s['X90']]) * qtp.tensor(
+#                    qtp.basis(2), qtp.basis(2))).full()),
+#     'X90ICZ': ([f'X90 {gate[0]}', f'{cz_pulse_name} {gate[0]} {gate[1]}'],
+#                (UCZ * qtp.tensor([U1s['X90'], U1s['I']]) * qtp.tensor(
+#                    qtp.basis(2), qtp.basis(2))).full()),
+#     'IX180CZ': ([f'X180 {gate[1]}', f'{cz_pulse_name} {gate[0]} {gate[1]}'],
+#                 (UCZ * qtp.tensor([U1s['I'], U1s['X180']]) * qtp.tensor(
+#                     qtp.basis(2), qtp.basis(2))).full()),
+#     'X180ICZ': ([f'X180 {gate[0]}', f'{cz_pulse_name} {gate[0]} {gate[1]}'],
+#                 (UCZ * qtp.tensor([U1s['X180'], U1s['I']]) * qtp.tensor(
+#                     qtp.basis(2), qtp.basis(2))).full()),
+#     'X180X180CZ': ([f'X180 {gate[1]}', f'X180s {gate[0]}',
+#                     f'{cz_pulse_name} {gate[0]} {gate[1]}'],
+#                    (UCZ * qtp.tensor([U1s['X180'], U1s['X180']]) * qtp.tensor(
+#                        qtp.basis(2), qtp.basis(2))).full()),
+# }
+# reload(ba)
+# reload(tda)
+# state_name = 'X180X180CZ'
+# _, rho_target = state_dict[state_name]
+#
+# preselection = hlp_mod.get_param_from_metadata_group(ts,
+#                                                      'use_preselection')  # True
+# use_cal_points = True
+# rots_basis = hlp_mod.get_param_from_metadata_group(ts, 'rots_basis')
+# channel_map = hlp_mod.get_param_from_metadata_group(ts, 'channel_map')
+# if hasattr(hlp_mod, 'get_instr_param_from_file'):
+#     get_instr_settings_func = hlp_mod.get_instr_param_from_file
+# else:
+#     get_instr_settings_func = hlp_mod.get_instr_param_from_hdf_file
+# try:
+#     thresholds = {qbn: get_instr_settings_func(qbn, 'acq_classifier_params',
+#                                                ts)['thresholds'][0]
+#                   for qbn in gate}
+# except KeyError:
+#     #     thresholds = hlp_mod.get_qb_thresholds_from_hdf_file(gate, ts)
+#     thresholds = {}
+#     for qbn in gate:
+#         acq_I_channel = get_instr_settings_func(qbn, 'acq_I_channel', ts)
+#         instr_uhf = get_instr_settings_func(qbn, 'instr_uhf', ts)
+#         thresholds[qbn] = get_instr_settings_func(instr_uhf,
+#                                                   f'qas_0_thresholds_{acq_I_channel}_level',
+#                                                   ts)
+# n_segments = len(rots_basis) ** 2 + (2 ** 2 if use_cal_points else 0)
+# # n_segments = hlp_mod.get_param_from_metadata_group(ts, 'n_segments')
+# MA = tda.StateTomographyAnalysis(t_start=ts, options_dict=dict(
+#     n_readouts=(2 if preselection else 1) * n_segments,
+#     thresholds=OrderedDict([(qb, thresholds[qb]) for qb in gate]),
+#     channel_map=OrderedDict([(qb, channel_map[qb]) for qb in gate]),
+#     cal_points=[
+#         OrderedDict([(channel_map[qb], [2 * i + 1 if preselection else i])
+#                      for qb in gate]) for i in np.arange(-4, 0)],
+#     #         meas_operators=[np.diag(1*(np.arange(4) == idx)) for idx in range(4)],
+#     data_type='singleshot',
+#     rho_target=qtp.Qobj(rho_target),
+#     basis_rots_str=rots_basis,
+#     covar_matrix=np.diag(np.ones(4)),
+#     mle=True,
+#     concurrence=False,
+#     use_preselection=preselection,
+#     data_filter=(
+#         lambda data: data[1:2 * len(rots_basis) ** 2 + 1:2]) if preselection \
+#         else (lambda data: data[:len(rots_basis) ** 2])
+# ))
+# os.startfile(a_tools.get_folder(ts))
---
+++ pycqed/tests/analysis/test_analysis_v3.py
@@ -0,0 +1,129 @@
+import pytest
+from pycqed.analysis import analysis_toolbox as a_tools
+from pycqed.analysis_v3 import plotting as plot_mod
+from pycqedscripts.scripts.characterization import randomized_benchmarking\
+    as rb_ana_full
+
+# Navigate to the folder where the testdata of this test is contained
+@pytest.fixture(scope="module", autouse=True)
+def set_module_data_dir(test_data_base_dir):
+    sub_directory = test_data_base_dir / 'setups/xld'
+    a_tools.datadir = str(sub_directory)
+    a_tools.fetch_data_dir = (r'Q:\Archive\Qudev107 - QComp PycQED data from '
+                              r'XLD\pydata')
+    return sub_directory
+
+def test_1qb_IRB_analysis():
+    timestamp = '20220203_183543'
+    plot_mod.get_default_plot_params()
+    pp = rb_ana_full.single_qubit_rb_analysis(
+        timestamp=timestamp,
+        extract_T2s=True,
+        renormalize_qb=(False,),
+        save_figures=True,
+        save_processed_data=False,
+    )
+
+def test_2qb_IRB_analysis():
+    timestamp = '20220722_131009'
+    plot_mod.get_default_plot_params()
+    pp = rb_ana_full.two_qubit_irb_analysis(
+        timestamp=timestamp,
+        save_figures=True,
+        save_processed_data=False,
+    )
+
+def test_1qb_XEB_analysis():
+    import \
+        pycqed.analysis_v3.cross_entropy_benchmarking_analysis as xeb_ana
+    timestamp = '20210722_120747'
+    plot_mod.get_default_plot_params()
+    renormalize = True  # renormalise qubit subspace pg + pg = 1
+    pp, meas_obj_names, cycles, nr_seq = xeb_ana.single_qubit_xeb_analysis(
+        timestamp, renormalize=renormalize, save=False)
+    xeb_ana.plot_porter_thomas_dist(pp.data_dict, renormalize=renormalize,
+                                    savefig=True)
+    xeb_ana.calculate_fidelities_purities_1qb(
+        pp.data_dict,
+        renormalize=renormalize,
+        # on the ro-corrected data; pass 'average_data' otherwise
+        data_key='correct_readout',
+    )
+    results_dict = xeb_ana.fit_plot_fidelity_purity(
+        pp.data_dict,
+        idx0f=0,  # fit fidelity starting at cycle index 0
+        idx0p=1,
+        # fit purity starting at 1; usually needs more randomisation
+        joint_processing=True,
+        log_scale=False,
+        savefig=True,
+    )
+    xeb_ana.fit_plot_leakage_1qb(pp.data_dict, meas_obj_names)
+
+def test_2qb_XEB_analysis():
+    import pycqed.analysis_v3.cross_entropy_benchmarking_analysis as xeb_ana
+    timestamp = '20220722_141951'
+    plot_mod.get_default_plot_params()
+    renormalize = True  # renormalise qubit subspace pgg + pge + peg + pee = 1
+    pp, meas_obj_names, cycles, nr_seq = xeb_ana.two_qubit_xeb_analysis(
+        timestamp, renormalize=renormalize, save=False)
+    xeb_ana.plot_porter_thomas_dist(pp.data_dict, renormalize=renormalize,
+                                    savefig=True)
+    xeb_ana.calculate_fidelities_purities_2qb(
+        pp.data_dict,
+        renormalize=renormalize,
+        # on the ro-corrected data; pass 'average_data' otherwise
+        data_key='correct_readout',
+    )
+    results_dict = xeb_ana.fit_plot_fidelity_purity(
+        pp.data_dict,
+        idx0f=0,  # fit fidelity starting at cycle index 0
+        idx0p=1,  # fit purity starting at 1; usually needs more randomisation
+        joint_processing=True,
+        log_scale=False,
+        savefig=True,
+    )
+    xeb_ana.fit_plot_leakage_2qb(pp.data_dict, meas_obj_names)
+
+# TODO 1qb State Tomography
+# from pycqedscripts.scripts.characterization.state_tomo import\
+#     do_state_tomo_analysis
+# timestamp = '20220723_171601'
+# plot_mod.get_default_plot_params()
+# psi_target = qt.qip.operations.gates.rx(np.pi)*qt.states.basis(2, 0)
+# rho_target = psi_target*psi_target.dag()
+# pp = do_state_tomo_analysis(timestamp=timestamp, rho_target=rho_target,
+#                             save_figures=True, save_processed_data=False)
+# os.startfile(a_tools.get_folder(timestamp))
+
+# TODO 2qb State Tomography
+# timestamp = '20220722_124116'
+# plot_mod.get_default_plot_params()
+# import qutip as qtp
+# from pycqed.analysis_v2 import tomography_qudev as tomo_ana
+# U1s = {pulse: tomo_ana.standard_qubit_pulses_to_rotations([(pulse,)])[0]
+#        for pulse in
+#        ['X90', 'Y90', 'mX90', 'mY90', 'I', 'X180', 'Y180', 'mY180',
+#         'mX180']}
+# UCZ = qtp.Qobj(np.diag([1, 1, 1, -1]), dims=[[2, 2], [2, 2]])
+# rho_target = (qtp.tensor([U1s['I'], U1s['mY90']]) * UCZ * qtp.tensor(
+#                      [U1s['mY90'], U1s['Y90']]) *
+#                   qtp.tensor(qtp.basis(2), qtp.basis(2))).full()
+# pp = do_state_tomo_analysis(timestamp=timestamp, rho_target=rho_target,
+#                             save_figures=True, save_processed_data=False)
+# os.startfile(a_tools.get_folder(timestamp))
+
+# TODO Process Tomography
+# from pycqedscripts.scripts.characterization.process_tomo import\
+#     do_process_tomo_analysis
+# # Produces very many plots!
+# timestamp = '20220722_124212'
+# os.startfile(a_tools.get_folder(timestamp))
+# plot_mod.get_default_plot_params()
+# pp = do_process_tomo_analysis(
+#     timestamp=timestamp,
+#     meas_obj_names=['qb10', 'qb11'],
+#     # save_figures_state_tomo=False,  # reduces nr of plots that are saved
+#     process_name='CZ',
+#     save_processed_data=False,
+# )
