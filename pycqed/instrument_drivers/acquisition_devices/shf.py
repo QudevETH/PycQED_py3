b61bd56c4a3e58afdc8461492be6d1cc0b55dd7f
Graham J. Norris <graham.norris@phys.ethz.ch>
2024-11-27 08:50:35 +0100

    shf: make allowed lo freqs labone version dependent

--- pycqed/instrument_drivers/acquisition_devices/shf.py
+++ pycqed/instrument_drivers/acquisition_devices/shf.py
@@ -1,3 +1,4 @@
+import logging
 import numpy as np
 from copy import deepcopy
 from qcodes.utils import validators
@@ -10,9 +11,18 @@ from pycqed.instrument_drivers.physical_instruments.ZurichInstruments.\
 from zhinst.qcodes import SHFQA as SHFQA_core
 from zhinst.qcodes import SHFQC as SHFQC_core
 from zhinst.qcodes import AveragingMode
+from pycqed.utilities import general as gen
 from pycqed.utilities.timer import Timer
-import logging
+
 log = logging.getLogger(__name__)
+
+# FIXME: remove this once 24.10 is the mandatory version
+zhinst_core_version = gen.get_zhinst_modules_versions()[0]["zhinst-core"]
+# e.g. "24.10.64896"
+if zhinst_core_version[:5] >= "24.10":
+    ALLOWED_LO_FREQS = np.arange(1e9, 8.0e9 + 1, 200e6)  # Include 8.0e9
+else:
+    ALLOWED_LO_FREQS = np.arange(1e9, 8.1e9 + 1, 100e6)  # Include 8.1e9
 
 
 class SHF_AcquisitionDevice(ZI_AcquisitionDevice, ZHInstMixin):
@@ -105,11 +115,12 @@ class SHF_AcquisitionDevice(ZI_AcquisitionDevice, ZHInstMixin):
 
         self.add_parameter(
             'allowed_lo_freqs',
-            initial_value=np.arange(1e9, 8.0e9, 200e6),
+            initial_value=ALLOWED_LO_FREQS,
             parameter_class=ManualParameter,
             docstring='List of values that the center frequency (LO) is '
                       'allowed to take. As of LabOne 24.10 this is '
-                      'limited to steps of 200 MHz.',
+                      'limited to steps of 200 MHz. Previously, it '
+                      "could take 100 MHz steps.",
             set_parser=lambda x: list(np.atleast_1d(x).flatten()),
             vals=validators.MultiType(validators.Lists(), validators.Arrays(),
                                       validators.Numbers()))
