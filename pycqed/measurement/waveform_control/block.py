af55f3a5937a29faf7ca6cb9d12cf75558b5a26d
Colin Scarato <cscarato@phys.ethz.ch>
2025-01-27 11:13:48 +0100

    bugfix cb, block: also modify func_for_op_code when using mathematical operations on ParametricValue and clarify the corresponding logic for mCZ gates

--- pycqed/measurement/waveform_control/block.py
+++ pycqed/measurement/waveform_control/block.py
@@ -356,11 +356,6 @@ class ParametricValue:
                 # Example: op_code = "Y:2*[v] qb1" -> "Y:90 qb1"
                 op_split = op_code.split(' ')
                 op_type = op_split[0].split(':')[0]
-                # Note for example that op_type might include an 'm' prefactor.
-                # This indicates a - sign in v_physical: op_code = "mY:90 qb1"
-                # yields the pulse amp v_physical corresponding to a -90 angle.
-                # In this case, func_for_op_code should not also contain
-                # this - sign, else the op_code would become "mY:-90 qb1".
                 v_op_code = self.func_for_op_code(v) \
                     if self.func_for_op_code else v
                 op_split[0] = f"{op_type}{v_op_code}"
@@ -386,6 +381,7 @@ class ParametricValue:
         pv = self._copy_self()
         pv.func_for_pulse_param = lambda x, f=pv.func_for_pulse_param: \
                 f(x) + other
+        pv.func_for_op_code = lambda x, f=pv.func_for_op_code: f(x) + other
         return pv
 
     __radd__ = __add__
@@ -394,23 +390,27 @@ class ParametricValue:
         pv = self._copy_self()
         pv.func_for_pulse_param = lambda x, f=pv.func_for_pulse_param: \
             f(x) - other
+        pv.func_for_op_code = lambda x, f=pv.func_for_op_code: f(x) - other
         return pv
 
     def __rsub__(self, other):
         pv = self._copy_self()
         pv.func_for_pulse_param = lambda x, f=pv.func_for_pulse_param: \
                 other - f(x)
+        pv.func_for_op_code = lambda x, f=pv.func_for_op_code: other - f(x)
         return pv
 
     def __neg__(self):
         pv = self._copy_self()
         pv.func_for_pulse_param = lambda x, f=pv.func_for_pulse_param: -f(x)
+        pv.func_for_op_code = lambda x, f=pv.func_for_op_code: -f(x)
         return pv
 
     def __mul__(self, other):
         pv = self._copy_self()
         pv.func_for_pulse_param = lambda x, f=pv.func_for_pulse_param: \
             f(x) * other
+        pv.func_for_op_code = lambda x, f=pv.func_for_op_code: f(x) * other
         return pv
 
     __rmul__ = __mul__
@@ -419,12 +419,14 @@ class ParametricValue:
         pv = self._copy_self()
         pv.func_for_pulse_param = lambda x, f=pv.func_for_pulse_param: \
             f(x) / other
+        pv.func_for_op_code = lambda x, f=pv.func_for_op_code: f(x) / other
         return pv
 
     def __rtruediv__(self, other):
         pv = self._copy_self()
         pv.func_for_pulse_param = lambda x, f=pv.func_for_pulse_param: \
             other / f(x)
+        pv.func_for_op_code = lambda x, f=pv.func_for_op_code: other / f(x)
         return pv
 
     def __repr__(self):
