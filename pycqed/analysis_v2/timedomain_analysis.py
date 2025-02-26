af0f4814267716d999ff9243f4938381b67428b0
Jonathan Trebst <jtrebst@ethz.ch>
2025-02-26 10:20:01 +0100

    cryoscope analysis in timedomain_analysis: added documentation and renamed variables to a more intuitive format
    Did not change functionality and tested the analysis again

--- pycqed/analysis_v2/timedomain_analysis.py
+++ pycqed/analysis_v2/timedomain_analysis.py
@@ -7892,15 +7892,19 @@ class CryoscopeAnalysis(DynamicPhaseAnalysis):
                     # the nyquist band at any step
                     df = []
                     prev_df = 0
-                    dij = self.get_param_value(
-                        'deltaf_indices_jumps') or ([], [])
+                    # In cases of large frequency changes between two truncation times
+                    # unwrapping mistakes can occur, these can corrected manually
+                    # {'indices': [list of indices], 'applied_shift': [correction applied at these indices]}
+                    shift_unwrapped_freqs = self.get_param_value(
+                        'shift_unwrapped_freqs') or {'indices': [], 'applied_shift':[]}
                     for dp, dt in zip(delta_phases_vals, delta_tau):
                         df.append(dp / (2 * np.pi * dt))
-                        df[-1] -= np.round((df[-1]-prev_df) * dt) / dt
-                        if len(df) in dij[0]:
-                            print(len(df))
-                            jump = dij[1][dij[0].index(len(df))]
-                            df[-1] += jump / dt
+                        df[-1] += np.round((prev_df - df[-1]) * dt) / dt
+                        if len(df) in shift_unwrapped_freqs['indices']:
+                            print(f'Manually shifted unwrapped frequency at index {len(df)}')
+                            freq_shift = shift_unwrapped_freqs['applied_shift'] \
+                                    [shift_unwrapped_freqs['indices'].index(len(df))]
+                            df[-1] += freq_shift / dt
                         prev_df = df[-1]
                     delta_phases_vals = np.array(df)*(2*np.pi*delta_tau)
                 else:
