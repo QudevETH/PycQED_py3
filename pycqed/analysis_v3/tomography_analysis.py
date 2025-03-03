3064ba3a3afc9dbe7ef5b34c4612a3a563ce96f4
Alonso Hernandez <ahernandez@phys.ethz.ch>
2025-03-03 12:48:05 +0100

    tomography_analysis: added log.warning about readout correction in convex MLE

--- pycqed/analysis_v3/tomography_analysis.py
+++ pycqed/analysis_v3/tomography_analysis.py
@@ -458,6 +458,8 @@ def density_matrices(data_dict,
             # FIXME: only supports readout correction so far.
             #  When there is no readout correction the covariance matrix
             #  is not square and I dont know how to interpret it.
+            log.warning('Convex MLE only supports tomography with readout '
+                        'correction so far.')
             
             rho_guess = hlp_mod.get_param('rho_guess', data_dict,
                                           default_value = None, **params)
