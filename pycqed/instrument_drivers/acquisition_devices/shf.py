719cf5653ed3310f126720110e4eec15fc4dce74
Christoph Hellings <chellings@phys.ethz.ch>
2025-05-13 16:00:26 +0200

    SHFQA/QC/SG: set default grid of center frequency to 200MHz
    This grid is used, in particular, for spectroscopy-type experiments.
    For other experiments, users need to adjust ro_fixed_lo_freq
    in the qubit objects.
    
    Using a 200 MHz grid is mandatory for firmware versions 24.10 and newer.
    In older versions, it is recommended for the following reason.
    
    For recent SHF devices with LPN synthesizer, the phase-frequency
    detector (PFD) in the PLL of the LPN works at 200 MHz in order
    to minimize phase noise. The output channels are not phase-aligned
    if the output frequency is not a multiple of 200 MHz (if multiple of
    100 MHz, a 180 degree phase shift between channels is possible).

--- pycqed/instrument_drivers/acquisition_devices/shf.py
+++ pycqed/instrument_drivers/acquisition_devices/shf.py
@@ -15,13 +15,7 @@ from pycqed.utilities.timer import Timer
 
 log = logging.getLogger(__name__)
 
-# FIXME: remove this once 24.10 is the mandatory version
-zhinst_core_version = gen.get_zhinst_modules_versions()[0]["zhinst-core"]
-# e.g. "24.10.64896"
-if zhinst_core_version[:5] >= "24.10":
-    ALLOWED_LO_FREQS = np.arange(1e9, 8.0e9 + 1, 200e6)  # Include 8.0e9
-else:
-    ALLOWED_LO_FREQS = np.arange(1e9, 8.1e9 + 1, 100e6)  # Include 8.1e9
+ALLOWED_LO_FREQS = np.arange(1e9, 8.0e9 + 1, 200e6)  # Include 8.0e9
 
 
 class SHF_AcquisitionDevice(ZI_AcquisitionDevice, ZHInstMixin):
