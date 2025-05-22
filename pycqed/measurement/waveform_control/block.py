29b3750e3ea8b640baee9d96e516e09f58a3a184
JanO <jobermeier@ethz.ch>
2025-05-22 14:19:44 +0200

    Update docstring

--- pycqed/measurement/waveform_control/block.py
+++ pycqed/measurement/waveform_control/block.py
@@ -438,10 +438,11 @@ def parse_pulse_search_pattern(pattern):
     """Parse strings/ints that represent a pulse search pattern
 
     Args:
-        pattern (str, int): the representation of the search pattern can be:
+        pattern (str, int, np.int_): the representation of the search pattern
+        can be:
           - a string as described in the docstring of Block.build,
             param sweep_dicts_list
-          - an int i, which will be interpreted as the string
+          - an int or np.int_ i, which will be interpreted as the string
             f'occurrence={i}'
           - the str 'all' for matching all pulses
 
